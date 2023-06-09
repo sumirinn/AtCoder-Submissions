242fb47d164e652a98fdfdd46aa0e88f4df6d6aa
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-06-09 09:48:34 +0000

    [abs] practice_1

---
+++ AtCoder submissions/abs/practice_1/practice_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a;
+  cin >> a;
+  
+  int b,c;
+  cin >> b;
+  cin >> c;
+  
+  string s;
+  cin >> s;
+    
+  cout << (a+b+c) << " " << s << endl;
+}
