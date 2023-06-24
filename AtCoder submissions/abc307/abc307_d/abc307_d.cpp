c4755f906eb98246fc931bfcbf0ba82d32a0da0a
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-06-24 15:55:37 +0000

    [abc307] abc307_d

--- AtCoder submissions/abc307/abc307_d/abc307_d.cpp
+++ AtCoder submissions/abc307/abc307_d/abc307_d.cpp
@@ -1,21 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main(){
     int n;
     cin >> n;
     string s;
     cin >> s;
     string ans;
-    int d=0;
-    for(auto c:s){
-        if(c==')'&&d>0){
-            while(ans.back()!='(')ans.pop_back();
+    int d = 0;
+    for(int i=0; i<n; i++){
+        char c;
+        c = s[i];
+        if(c==')' && d>0){
+            while(ans.back()!='('){
+                ans.pop_back();
+            }
             ans.pop_back();
             d--;
         }else{
-            ans+=c;
-            if(c=='(')d++;
+            ans += c;
+            if(c =='('){
+                d++;
+            }
         }
     }
     cout << ans << endl;
