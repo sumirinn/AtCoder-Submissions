3827f4df95204945b5323a93cab7033132f46100
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-08-11 23:35:03 +0000

    [APG4b] APG4b_ce

--- AtCoder submissions/APG4b/APG4b_ce/APG4b_ce.cpp
+++ AtCoder submissions/APG4b/APG4b_ce/APG4b_ce.cpp
@@ -1,38 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main() {
-  int N, M;
-  cin >> N >> M;
-  vector<int> A(M), B(M);
-  for (int i = 0; i < M; i++) {
-    cin >> A.at(i) >> B.at(i);
-  }
- 
-  // ここにプログラムを追記
-  // (ここで"試合結果の表"の2次元配列を宣言)
-  
-  vector<vector<char>> v(N, vector<char>(N));
-  
-  for(int i=0; i<N; i++){
-    for(int j=0; j<N; j++){
-      v[i][j] = '-';
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(m), b(m);
+    for(int i=0; i<m; i++){
+        cin >> a[i] >> b[i];
     }
-  }
-  
-  for(int k=0; k<M; k++){
-    v[A[k]-1][B[k]-1] = 'o';
-    v[B[k]-1][A[k]-1] = 'x';
-  }
-  
-  for(int l=0; l<N; l++){
-    for(int a=0; a<N; a++){
-      cout << v[l][a];
-      if(a==N-1){
-        cout << endl;
-      }else{
-        cout << ' ';
-      }
+
+    vector<vector<char>> v(n, vector<char>(n, '-'));
+
+    for(int i=0; i<m; i++){
+        v[a[i]-1][b[i]-1] = 'o';
+        v[b[i]-1][a[i]-1] = 'x';
     }
-  }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << v[i][j];
+
+            if(j==n-1){
+                cout << endl;
+            }else{
+                cout << " ";
+            }
+        }    
+    }
 }
