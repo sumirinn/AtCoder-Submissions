e4e30eb5e2078e0f2de9c7558052f15875833eb8
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-08-18 16:56:24 +0000

    [tessoku-book] tessoku_book_cg

--- AtCoder submissions/tessoku-book/tessoku_book_cg/tessoku_book_cg.cpp
+++ AtCoder submissions/tessoku-book/tessoku_book_cg/tessoku_book_cg.cpp
@@ -1,38 +1,34 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
- 
-// 入力で与えられる変数
-int N, X[100009], Y[100009];
-int Q, A[100009], B[100009], C[100009], D[100009];
- 
-// 各座標にある点の数 S[i][j]、二次元累積和 T[i][j]
-int S[1509][1509];
-int T[1509][1509];
- 
-int main() {
-	// 入力
-	cin >> N;
-	for (int i = 1; i <= N; i++) cin >> X[i] >> Y[i];
-	cin >> Q;
-	for (int i = 1; i <= Q; i++) cin >> A[i] >> B[i] >> C[i] >> D[i];
- 
-	// 各座標にある点の数を数える
-	for (int i = 1; i <= N; i++) S[X[i]][Y[i]] += 1;
- 
-	// 累積和をとる
-	for (int i = 0; i <= 1500; i++) {
-		for (int j = 0; j <= 1500; j++) T[i][j] = 0;
-	}
-	for (int i = 1; i <= 1500; i++) {
-		for (int j = 1; j <= 1500; j++) T[i][j] = T[i][j - 1] + S[i][j];
-	}
-	for (int i = 1; i <= 1500; i++) {
-		for (int j = 1; j <= 1500; j++) T[i][j] = T[i - 1][j] + T[i][j];
-	}
- 
-	// 答えを求める
-	for (int i = 1; i <= Q; i++) {
-		cout << T[C[i]][D[i]] + T[A[i] - 1][B[i] - 1] - T[A[i] - 1][D[i]] - T[C[i]][B[i] - 1] << endl;
-	}
-	return 0;
+
+int n, X[100009], Y[100009];
+int Z[1509][1509], ANS[1509][1509];
+int q, A[100009], B[100009], C[100009], D[100009];
+
+int main(){
+  cin >> n;
+  for(int i=1; i<=n; i++) cin >> X[i] >> Y[i];
+  cin >> q;
+  for(int i=1; i<=q; i++) cin >> A[i] >> B[i] >> C[i] >> D[i];
+  
+  for(int i=0; i<=1500; i++){
+    for(int j=0; j<=1500; j++){
+      Z[i][j] = 0;
+      ANS[i][j] = 0;
+    }
+  }
+  
+  for(int i=1; i<=n; i++) Z[X[i]][Y[i]] += 1;
+  
+  for(int i=1; i<=1500; i++){
+    for(int j=1; j<=1500; j++) ANS[i][j] = ANS[i][j-1] + Z[i][j];
+  }
+  
+  for(int i=1; i<=1500; i++){
+    for(int j=1; j<=1500; j++) ANS[i][j] = ANS[i-1][j] + ANS[i][j];
+  }
+  
+  for(int i=1; i<=q; i++){
+    cout << ANS[C[i]][D[i]] + ANS[A[i]-1][B[i]-1] -ANS[A[i]-1][D[i]] - ANS[C[i]][B[i]-1] << endl;
+  }
 }
