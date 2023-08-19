d4f1fdb021ccc5acaff4dee67fc01cca6db3c248
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-08-19 03:04:10 +0000

    [tessoku-book] tessoku_book_ck

---
+++ AtCoder submissions/tessoku-book/tessoku_book_ck/tessoku_book_ck.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+double f(double x){
+  return x*x*x + x;
+}
+
+int main(){
+  int n;
+  cin >> n;
+  
+  double left = 0, right = 100, mid;
+  for(int i=0; i<=20; i++){
+    mid = (left + right) / 2.0;
+    double val = f(mid);
+    
+    if(val>1.0*n) right =mid;
+    else left = mid;
+  }
+  
+  printf("%.12lf\n", mid);
+}
+  
