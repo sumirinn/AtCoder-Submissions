ec76617891bc60e625bf92b29a5acb9304a6d591
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-08-29 05:16:10 +0000

    [tessoku-book] tessoku_book_ct

---
+++ AtCoder submissions/tessoku-book/tessoku_book_ct/tessoku_book_ct.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+//答え見た
+int N;
+int dp[1009][1009];
+string S;
+
+int main(){
+    cin >> N;
+    cin >> S;
+
+    for(int i=0; i<N; i++) dp[i][i] = 1;
+    for(int i=0; i<N-1; i++){
+        if(S[i]==S[i+1]) dp[i][i+1] = 2;
+        else dp[i][i+1] = 1;
+    }
+
+    for(int LEN=2; LEN<=N-1; LEN++){
+        for(int l=0; l<N-LEN; l++){
+            int r = l + LEN;
+
+            if(S[l]==S[r]) dp[l][r] = max({dp[l][r-1], dp[l+1][r], dp[l+1][r-1]+2});
+            else dp[l][r] = max({dp[l][r-1], dp[l+1][r]});
+        }
+    }
+
+    cout << dp[0][N-1] << endl;
+}
