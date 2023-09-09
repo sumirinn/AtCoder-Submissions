6f4f13b435e76dc8b65a62a190686757f08909e8
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-09-09 01:56:01 +0000

    [tessoku-book] tessoku_book_dr

---
+++ AtCoder submissions/tessoku-book/tessoku_book_dr/tessoku_book_dr.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+long long a, b, c;
+
+int main(){
+    cin >> a >> b >> c;
+
+    if(a+b+c==0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
