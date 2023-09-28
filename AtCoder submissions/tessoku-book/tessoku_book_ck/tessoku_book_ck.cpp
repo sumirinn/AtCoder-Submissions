2424cde1b48107d055ae9a091643e685a9cc0850
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-09-28 02:23:08 +0000

    [tessoku-book] tessoku_book_ck

--- AtCoder submissions/tessoku-book/tessoku_book_ck/tessoku_book_ck.cpp
+++ AtCoder submissions/tessoku-book/tessoku_book_ck/tessoku_book_ck.cpp
@@ -1,24 +1,28 @@
-#include <bits/stdc++.h>
-#include <algorithm>
+#include <iostream>
 using namespace std;
-
-double f(double x){
-  return x*x*x + x;
+ 
+// 関数 f
+double f(double x) {
+	return x * x * x + x;
 }
-
-int main(){
-  int n;
-  cin >> n;
-  
-  double left = 0, right = 100, mid;
-  for(int i=0; i<=20; i++){
-    mid = (left + right) / 2.0;
-    double val = f(mid);
-    
-    if(val>1.0*n) right =mid;
-    else left = mid;
-  }
-  
-  cout << mid << endl;
+ 
+int main() {
+	// 入力
+	int N;
+	cin >> N;
+ 
+	// 二分探索
+	double Left = 0, Right = 100, Mid;
+	for (int i = 0; i < 20; i++) {
+		Mid = (Left + Right) / 2.0;
+		double val = f(Mid);
+ 
+		// 探索範囲を絞る
+		if (val > 1.0 * N) Right = Mid; // 左半分に絞られる
+		else Left = Mid; // 右半分に絞られる
+	}
+ 
+	// 出力
+	cout << Mid << endl;
+	return 0;
 }
-  
