ac8dd87f2dbfb5ef4bfe09d377ee4897ef490b61
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-09-30 12:03:20 +0000

    [abc322] abc322_a

---
+++ AtCoder submissions/abc322/abc322_a/abc322_a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int n;
+string s;
+
+int main(){
+    cin >> n >> s;
+    
+    int ans = -1;
+    
+    for(int i=0; i<s.size()-2; i++){
+        if(s[i]=='A' && s[i+1]=='B'&& s[i+2]=='C'){
+            ans = i+1;
+            break;
+        }
+    }
+    
+    cout << ans << endl;
+}
