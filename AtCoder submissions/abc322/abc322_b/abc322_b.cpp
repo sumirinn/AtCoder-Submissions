7530976a4b04936a0c9bd4bc27c543be980474b8
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-10-02 21:29:50 +0000

    [abc322] abc322_b

--- AtCoder submissions/abc322/abc322_b/abc322_b.cpp
+++ AtCoder submissions/abc322/abc322_b/abc322_b.cpp
@@ -7,26 +7,23 @@ using namespace std;
 
 int n, m;
 string s, t;
-bool mae=true, usiro=true;
 
 int main(){
     cin >> n >> m >> s >> t;
     
-    for(int i=0; i<n; i++){
-        if(s[i]!=t[i]) mae = false;
+    bool ans1 = true;
+    for(int i=0; i<s.size(); i++){
+        if(s[i]!=t[i]) ans1 = false;
     }
     
-    for(int i=n-1; i>=0; i--){
-        if(s[i]!=t[m-n+i]) usiro = false;
+    bool ans2 = true;
+    for(int i=0; i<s.size(); i++){
+        if(s[i]!=t[m-n+i]) ans2 = false;
     }
     
-    int ans = 100;
-    
-    if(mae==true && usiro==true) ans = 0;
-    if(mae==true && usiro==false) ans = 1;
-    if(mae==false && usiro==true) ans = 2;
-    if(mae==false && usiro==false) ans = 3;
-    
-    cout << ans << endl;
-    
+    if(ans1){
+        cout << (ans2 ? 0 : 1) << endl;
+    }else{
+        cout << (ans2 ? 2 : 3) << endl;
+    }
 }
