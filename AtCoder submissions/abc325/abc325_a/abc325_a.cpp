eab13ff674b656a7c818a3daca752ca44cf912dd
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-10-28 07:56:50 +0000

    [abc325] abc325_a

---
+++ AtCoder submissions/abc325/abc325_a/abc325_a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <queue>
+using namespace std;
+
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+
+string s;
+
+int main(){
+    cin >> s;
+    cout << s << " " << "san" << endl;
+}
