63d445850f67442bf3243f5d352ad96a885fffe2
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-12-23 12:01:50 +0000

    [abc334] abc334_a

---
+++ AtCoder submissions/abc334/abc334_a/abc334_a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repp(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int b, g;
+
+int main(){
+    cin >> b >> g;
+
+    if(b>g) cout << "Bat" << endl;
+    else cout << "Glove" << endl;
+
+}
