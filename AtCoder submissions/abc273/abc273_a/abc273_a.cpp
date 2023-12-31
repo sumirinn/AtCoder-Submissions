f35abb1d196c23abcdc784da63b11a1597f19ecf
rintaro <136178719+sumirinn@users.noreply.github.com>
2023-12-31 08:35:04 +0000

    [abc273] abc273_a

---
+++ AtCoder submissions/abc273/abc273_a/abc273_a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repp(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int n;
+
+int main(){
+    cin >> n;
+
+    int ans = 1;
+    rep(i, n) ans *= i;
+    cout << ans << endl;
+}
