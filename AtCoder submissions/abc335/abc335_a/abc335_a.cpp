d55fbec091b8bcffc215dc3d863d9a9d66a31434
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-01-06 12:01:15 +0000

    [abc335] abc335_a

---
+++ AtCoder submissions/abc335/abc335_a/abc335_a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repp(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+//97~122(a~z)
+
+int main(){
+    string s;
+    cin >> s;
+
+    s[s.size()-1] = '4';
+    cout << s << endl;
+}
