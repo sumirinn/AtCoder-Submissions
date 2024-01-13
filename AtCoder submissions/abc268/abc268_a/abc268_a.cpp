a1f7ec21e8aa50efb210fb8532ccba84fd6c3246
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-01-13 04:49:59 +0000

    [abc268] abc268_a

---
+++ AtCoder submissions/abc268/abc268_a/abc268_a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repp(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<ll, ll>;
+//97~122(a~z)
+
+int main(){
+    int a;
+    set<int> s;
+    rep(i, 5){
+        cin >> a;
+        s.insert(a);
+    }
+    cout << s.size() << endl;
+}
