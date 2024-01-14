62ec6ebbcd8fbc45e91bcdbfa52a556388103270
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-01-14 12:01:23 +0000

    [abc336] abc336_a

---
+++ AtCoder submissions/abc336/abc336_a/abc336_a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repp(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, -1, 0, 1};
+//97~122(a~z)
+
+int main(){
+    int n;
+    cin >> n;
+    cout << 'L';
+    rep(i, n) cout << 'o';
+    cout << "ng" <<endl;
+}
