b8bd41e535e634d2b756785c710b3aac35f93c53
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-01-19 05:14:58 +0000

    [abc266] abc266_a

---
+++ AtCoder submissions/abc266/abc266_a/abc266_a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repp(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint998244353;
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, -1, 0, 1};
+//97~122(a~z)
+
+int main(){
+    string s;
+    cin >> s;
+
+    cout << s[s.size()/2] << endl;
+}
