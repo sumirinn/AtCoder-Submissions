3330097c426df82e6edd048c38c5a0807b4e9efb
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-01-21 17:58:25 +0000

    [abc264] abc264_b

---
+++ AtCoder submissions/abc264/abc264_b/abc264_b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repp(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint998244353;
+void chmax(ll& x, ll y) {x = max(x, y);}
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int inf = 1001001001;
+//97~122(a~z)
+
+int main(){
+    int r, c;
+    cin >> r >> c;
+    int d = max(abs(r-8), abs(c-8));
+    if(d%2) cout << "black" << endl;
+    else cout << "white" << endl;
+}
