7e534dda592e7794d37aa8414cfbc9e3323645cc
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-02-10 12:04:37 +0000

    [abc340] abc340_b

---
+++ AtCoder submissions/abc340/abc340_b/abc340_b.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repp(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint998244353;
+void chmax(ll& x, ll y) {x = max(x, y);}
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, 1, 0, -1};
+const int inf = 1001001001;
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    int q; cin >> q;
+    vector<ll> a;
+    rep(qi, q){
+        int m; cin >> m;
+        if(m==1){
+            ll x; cin >> x;
+            a.push_back(x);
+        }
+        if(m==2){
+            int k; cin >> k;
+            cout << a[a.size()-k] << endl;
+        }
+    }
+}
