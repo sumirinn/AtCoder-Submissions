f6754055eb5f5eb87dccba438d60a3e307e1ff0d
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-02-15 09:28:38 +0000

    [abc282] abc282_c

---
+++ AtCoder submissions/abc282/abc282_c/abc282_c.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint998244353;
+void chmax(ll& x, ll y) {x = max(x, y);}
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, 1, 0, -1};
+const int inf = 1001001001;
+const ll INF = 1e18;
+ll c3(ll n) { return n*(n-1)*(n-2) / 6; }
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    int n;
+    cin >> n;
+    vector<char> s(n);
+    rep(i, n) cin >> s[i];
+
+    int cnt = 0;
+    rep(i, n){
+        if(s[i]=='"') cnt++;
+        if(cnt%2==0 && s[i]==',') s[i] = '.';
+    }
+
+
+
+    rep(i, n) cout << s[i];
+    cout << endl;
+}
