254268419ddd62e104c1f378b7f62d246cc0dfb0
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-02-15 10:30:16 +0000

    [abc240] abc240_a

---
+++ AtCoder submissions/abc240/abc240_a/abc240_a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint998244353;
+void chmax(ll& x, ll y) {x = max(x, y);}
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, 1, 0, -1};
+const int inf = 1001001001;
+const ll INF = 1e18;
+ll c3(ll n) { return n*(n-1)*(n-2) / 6; }
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    int a, b; 
+    cin >> a >> b;
+    if((b-a)%9==0 || (b-a)%9==1) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
