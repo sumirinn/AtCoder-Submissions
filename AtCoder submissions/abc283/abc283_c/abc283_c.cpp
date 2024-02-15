28bc1817e17582fae4fd40bd86404f7503abce78
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-02-15 09:21:41 +0000

    [abc283] abc283_c

---
+++ AtCoder submissions/abc283/abc283_c/abc283_c.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint998244353;
+void chmax(ll& x, ll y) {x = max(x, y);}
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, 1, 0, -1};
+const int inf = 1001001001;
+const ll INF = 1e18;
+ll c3(ll n) { return n*(n-1)*(n-2) / 6; }
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    int pos = 0;
+    int ans = s.size();
+    while(pos<n){
+        if(pos<n && s[pos]=='0' && s[pos+1]=='0'){
+            pos++;
+            ans--;
+        }
+        pos++;
+    }
+    
+    cout << ans << endl;
+}
