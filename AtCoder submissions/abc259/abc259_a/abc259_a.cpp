7415c75ec88aa55e4c536dc47ccb40574241e0cb
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-02-15 22:29:22 +0000

    [abc259] abc259_a

---
+++ AtCoder submissions/abc259/abc259_a/abc259_a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint998244353;
+void chmax(ll& x, ll y) {x = max(x, y);}
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, 1, 0, -1};
+const int inf = 1001001001;
+const ll INF = 1e18;
+ll c3(ll n) { return n*(n-1)*(n-2) / 6; }
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    int n, m, x, t, d;
+    cin >> n >> m >> x >> t >> d;
+    if(m<=x){
+        cout << t - (x-m) * d << endl;
+        return 0;
+    }
+    cout << t << endl;
+}
