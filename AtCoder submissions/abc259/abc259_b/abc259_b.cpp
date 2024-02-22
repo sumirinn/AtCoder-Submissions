fb8b3754100ebb14cfce4cf5a52c3e671b54f086
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-02-22 03:53:38 +0000

    [abc259] abc259_b

---
+++ AtCoder submissions/abc259/abc259_b/abc259_b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using MP = map<int, vector<int>>;
+using mint = modint998244353;
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+//const int di[] = {0, 1, 0, -1};
+//const int dj[] = {1, 0, -1, 0};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+const int inf = 1001001001;
+const ll INF = 1e18;
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+//97~122(a~z),65~90(A~Z)
+
+
+int main(){
+    int a, b, d;
+    cin >> a >> b >> d;
+
+    double theta = atan2(b, a);
+    double r = sqrt(a*a+b*b);
+    const double pi = acos(-1);
+    theta += d/180.0*pi;
+    double x = r*cos(theta);
+    double y = r*sin(theta);
+    cout << fixed << setprecision(20) << x << " " << y << endl;
+}
