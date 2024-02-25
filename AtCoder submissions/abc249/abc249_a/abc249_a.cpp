059e03a0b6d3026aab3e1c2b891476f892911c65
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-02-25 23:39:38 +0000

    [abc249] abc249_a

---
+++ AtCoder submissions/abc249/abc249_a/abc249_a.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
+using ll = long long;
+using P = pair<ll, int>;
+using MP = map<int, vector<int>>;
+using mint = modint998244353;
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {0, 1, 0, -1};
+const int dj[] = {1, 0, -1, 0};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+const int inf = 1001001001;
+const ll INF = 1e18;
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    int a, b, c, d, e, f, x;
+    cin >> a >> b >> c >> d >> e >> f>> x;
+
+    int t=0, D=0, y=x;
+    while(x>0){
+        t += b * min(a,x);
+        x -= a+c;
+    }
+    while(y>0){
+        D += e * min(d, y);
+        y -= d+f;
+        //cout << D << endl;
+    }
+
+    //cout << t << " " << D << endl;
+
+    if(t>D) cout << "Takahashi" << endl;
+    if(t==D) cout << "Draw" << endl;
+    if(t<D) cout << "Aoki" << endl;
+}
