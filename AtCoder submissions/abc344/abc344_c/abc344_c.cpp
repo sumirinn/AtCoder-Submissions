defdb855b0c75086f7f25c8f5c32e6d817a109fb
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-03-09 12:11:49 +0000

    [abc344] abc344_c

---
+++ AtCoder submissions/abc344/abc344_c/abc344_c.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
+using ll = long long;
+using MP = map<int, vector<int>>;
+using mint = modint998244353;
+using P = pair<ll, int>;
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+const int inf = 1001001001;
+const ll INF = 1e18;
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    int n, m, l, q;
+
+    cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+
+    cin >> m;
+    vector<ll> b(m);
+    rep(i,m) cin >> b[i];
+
+    cin >> l;
+    vector<ll> c(l);
+    rep(i,l) cin >> c[i];
+
+    set<ll> st;
+    rep(i,n)rep(j,m)rep(k,l) st.insert(a[i]+b[j]+c[k]);
+
+    cin >> q;
+    rep(qi,q){
+        ll x;
+        cin >> x;
+        if(st.count(x)) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+}
