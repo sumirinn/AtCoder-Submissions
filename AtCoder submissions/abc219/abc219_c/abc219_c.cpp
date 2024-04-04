1cb7ba6409a67ac749ffd032c028bd94b2f4525f
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-04-04 09:51:18 +0000

    [abc219] abc219_c

---
+++ AtCoder submissions/abc219/abc219_c/abc219_c.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+using ll = long long; using pii = pair<int, int>;
+using pll = pair<ll, ll>;  using pdd = pair<double, double>;
+const int inf = 1001001001; const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
+//auto nex = calc_next で取得すると楽。
+vector<vector<int> > calc_next(const string &S) {
+    int N = (int)S.size();
+    vector<vector<int> > res(N+1, vector<int>(26, N));
+    for (int i = N-1; i >= 0; --i) {
+        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
+        res[i][S[i]-'a'] = i;
+    }
+    return res;
+}
+//comb(n,k)でnCkになる。
+struct modinv {
+    int n; vector<mint> d;
+    modinv(): n(2), d({0,1}) {}
+    mint operator()(int i) {
+    while (n <= i) d.push_back(-d[mint::mod()%n]*(mint::mod()/n)), ++n;
+    return d[i];
+    }
+    mint operator[](int i) const { return d[i];}
+} invs;
+struct modfact {
+    int n; vector<mint> d;
+    modfact(): n(2), d({1,1}) {}
+    mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*n), ++n;
+    return d[i];
+    }
+    mint operator[](int i) const { return d[i];}
+} facts;
+struct modfactinv {
+    int n; vector<mint> d;
+    modfactinv(): n(2), d({1,1}) {}
+    mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*invs(n)), ++n;
+    return d[i];
+    }
+    mint operator[](int i) const { return d[i];}
+} ifacts;
+mint comb(int n, int k) {
+    if (n < k || k < 0) return 0;
+    return facts(n)*ifacts(k)*ifacts(n-k);
+}
+//素数判定  
+bool isPrime(int x){
+    for(int i=2; i*i<=x; i++) if(x%i==0) return false;
+    return true;
+}
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<string, int>;
+using MP = map<int, vector<int>>;
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    string x;
+    int n;
+    cin >> x >> n;
+    map<char, int> mp;
+    rep(i,x.size()) mp[x[i]] = i;
+    vector<string> s(n);
+    rep(i,n) cin >> s[i];
+    vector<P> c(n);
+    rep(i,n){
+        c[i].second = i;
+        rep(j,s[i].size()){
+            c[i].first += char(mp[s[i][j]] + 97);
+        }
+    }
+
+    sort(c.begin(), c.end());
+    rep(i,n) cout << s[c[i].second] << endl;
+}
