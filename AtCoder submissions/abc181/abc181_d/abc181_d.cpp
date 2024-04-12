efc786a0c5db049fcc321dacd43022380af5ef95
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-04-12 14:57:42 +0000

    [abc181] abc181_d

---
+++ AtCoder submissions/abc181/abc181_d/abc181_d.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+using ll = long long; using db = double;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  using pdd = pair<double, double>;
+const int inf = 1001001001; const ll INF = 3e18;
+//using mint = modint998244353;
+using mint = modint1000000007;
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
+//auto nex = calc_next で取得すると楽。
+vector<vector<int> > calc_next(const string &S) {
+    int N = (int)S.size();
+    vector<vector<int> > res(N+1, vector<int>(26, N));
+    for (int i = N-1; i >= 0; --i) {
+        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
+        res[i][S[i]-'a'] = i;
+    }
+    return res;
+}
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<string, int>;
+using MP = map<int, vector<int>>;
+void chmax(int& x, int y) {x = max(x, y);} // change max
+void chmin(int& x, int y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector<int> cnt(10);
+    rep(i,n) cnt[s[i]-'0']++;
+    const int m = 9;
+    if(n==1){
+        if(cnt[8]==1) cout << "Yes" << endl;
+        else cout << "No" << endl;
+        return 0;
+    }
+    if(n==2){
+        repp(i,m){
+            repp(j,m){
+                cnt[i]--;
+                cnt[j]--;
+                bool ok = true;
+                if(cnt[i]<0 || cnt[j]<0) ok = false;
+                cnt[i]++;
+                cnt[j]++;
+                if(ok){
+                    int a = i*10 + j;
+                    if(a%8==0){
+                        cout << "Yes" << endl;
+                        return 0;
+                    }
+                }
+            }
+        }
+        cout << "No" << endl;
+        return 0;
+    }
+    repp(i,m){
+        repp(j,m){
+            repp(k,m){
+                cnt[i]--;
+                cnt[j]--;
+                cnt[k]--;
+                bool ok = true;
+                if(cnt[i]<0 || cnt[j]<0 || cnt[k]<0) ok = false;
+                cnt[i]++;
+                cnt[j]++;
+                cnt[k]++;
+                if(ok){
+                    int a = i*100 + j*10 + k;
+                    if(a%8==0){
+                        cout << "Yes" << endl;
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+    cout << "No" << endl;
+}
