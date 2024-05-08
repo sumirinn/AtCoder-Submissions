b6bce313151b28dda08c5bb4dbf6eebd2302d842
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-05-08 12:33:34 +0000

    [abc187] abc187_e

---
+++ AtCoder submissions/abc187/abc187_e/abc187_e.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+using ll = long long; using db = double;
+using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+const int inf = 1001001001; const ll INF = 3e18;
+//using mint = modint998244353;
+using mint = modint1000000007;
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
+//auto nex = calc_next で取得すると楽。
+vector<vector<int> > calc_next(const string &S) {
+    int N = (int)S.size();
+    vector<vector<int> > res(N+1, vector<int>(26, N));
+    for (int i = N-1; i >= 0; --i) {
+        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
+        res[i][S[i]-'a'] = i;
+    }
+    return res;
+}
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<string, int>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y;
+    cin >> x >> y;
+    return C(x,y);
+}
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(int& x, int y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<pii> es(n-1);
+    vector<vector<int>> to(n);
+    rep(i,n-1){
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        to[a].push_back(b);
+        to[b].push_back(a);
+        es[i] = {a, b};
+    }
+
+    vector<int> dist(n,-1);
+    dist[0] = 0;
+    queue<int> q;
+    q.push(0);
+    while(!q.empty()){
+        int v = q.front();
+        q.pop();
+        for(int u : to[v]){
+            if(dist[u]!=-1) continue;
+            dist[u] = dist[v] + 1;
+            q.push(u); 
+        }
+    }
+
+    vector<ll> s(n,0);
+
+    int Q;
+    cin >> Q;
+    rep(qi,Q){
+        int t, e;
+        ll x;
+        cin >> t >> e >> x;
+        e--;
+        auto [a,b] = es[e];
+        if(dist[a]>dist[b]){
+            swap(a,b);
+            t ^= 3;
+        }
+        if(t==1){
+            s[0] += x;
+            s[b] -= x;
+        }
+        if(t==2){
+            s[b] += x;
+        }
+    }
+
+    q.push(0);
+    while(!q.empty()){
+        int v = q.front();
+        q.pop();
+        for(int u : to[v]){
+            if(dist[v]>dist[u]) continue;
+            s[u] += s[v];
+            q.push(u); 
+        }
+    }
+
+    rep(i,n) cout << s[i] << endl;
+}
