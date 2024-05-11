234db632bfb35d861bfbe2dc1e639ea9bd1e1f09
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-05-11 10:57:56 +0000

    [abc220] abc220_f

---
+++ AtCoder submissions/abc220/abc220_f/abc220_f.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+using ll = long long; using db = double;
+using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
+//auto nex = calc_next で取得すると楽。
+vector<vector<int> > calc_next(const string &S) {
+    int N = (int)S.size();
+    vector<vector<int> > res(N+1, vector<int>(26, N));
+    for (int i = N-1; i >= 0; --i) {
+        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
+        res[i][S[i]-'a'] = i;
+    }
+    return res;
+}
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<string, int>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y;
+    cin >> x >> y;
+    return C(x,y);
+}
+struct Edge {
+    int u, v;
+    ll w;
+    Edge(){}
+    Edge(int u, int v, ll w): u(u), v(v), w(w){}
+};
+void chmax(int& x, int y) {x = max(x, y);} // change max
+void chmin(int& x, int y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<vector<int>> to(n);
+    rep(i,n-1){
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        to[u].push_back(v);
+        to[v].push_back(u);
+    }
+
+    vector<int> t(n);
+    vector<ll> ans(n);
+    auto dfs1 =[&](auto dfs1, int v, int p=-1) -> ll {
+        ll res = 0;
+        t[v] = 1;
+        for(int u : to[v]){
+            if(u==p) continue;
+            res += dfs1(dfs1,u,v) + t[u];
+            t[v] += t[u];
+        }
+        return res;
+    };
+
+    ans[0] = dfs1(dfs1,0);
+    //rep(i,n) cout << t[i] << endl;
+    auto dfs2 =[&](auto dfs2, int v, int p=-1) -> void {
+        for(int u : to[v]){
+            if(u==p) continue;
+            ans[u] = ans[v] + (n-t[u]) - t[u];
+            dfs2(dfs2,u,v);
+        }
+    };
+    dfs2(dfs2,0);
+    rep(i,n) cout << ans[i] << endl;
+}
