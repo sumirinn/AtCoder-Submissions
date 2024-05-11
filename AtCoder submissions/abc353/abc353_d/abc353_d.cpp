2a01bcbdc63315c8abd86d1838e6652a6921e6ae
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-05-11 12:39:40 +0000

    [abc353] abc353_d

---
+++ AtCoder submissions/abc353/abc353_d/abc353_d.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+using ll = long long; using db = double;
+using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
+//auto nex = calc_next で取得すると楽。
+vector<vector<int> > calc_next(const string &S) {
+    int N = (int)S.size();
+    vector<vector<int> > res(N+1, vector<int>(26, N));
+    for (int i = N-1; i >= 0; --i) {
+        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
+        res[i][S[i]-'a'] = i;
+    }
+    return res;
+}
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<string, int>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y;
+    cin >> x >> y;
+    return C(x,y);
+}
+struct Edge {
+    int u, v;
+    ll w;
+    Edge(){}
+    Edge(int u, int v, ll w): u(u), v(v), w(w){}
+};
+void chmax(int& x, int y) {x = max(x, y);} // change max
+void chmin(int& x, int y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    int m = 11;
+    ll M = 998244353;
+    vector<mint> p(m);
+    p[0] = 1;
+    rep(i,m-1) p[i+1] = p[i]*10;
+    vector<vector<int>> bi(n, vector<int>(m,0));
+
+    rep(i,n){
+        int d = 0;
+        ll A = a[i];
+        while(A!=0){
+            d++;
+            A /= 10;
+        }
+        bi[i][d]++;
+    }
+
+    for(int i=n-1; i>0; i--){
+        rep(j,m) bi[i-1][j] += bi[i][j];
+    }
+
+    mint ans = 0;
+    rep(i,n-1){
+        rep(j,m){
+            if(bi[i+1][j]){
+                mint now = (mint)a[i]*p[j];
+                ans += now*(mint)bi[i+1][j];
+            }
+        }
+    }
+    for(int i=n-1; i>0; i--){
+        ans += (mint)a[i]*(mint)(i);
+    }
+
+    cout << ans.val() << endl;
+}
