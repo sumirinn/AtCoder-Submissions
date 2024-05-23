f78bd1a08e25c1e1522c99fca6210d8d50c3dd96
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-05-23 09:55:47 +0000

    [abc146] abc146_d

---
+++ AtCoder submissions/abc146/abc146_d/abc146_d.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+//#define int long long
+//typedef long long int;
+using ll = long long; using db = double;
+using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる。
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
+//auto nex = calc_next で取得すると楽。
+vector<vector<int> > calc_next(const string &S) {
+    int N = (int)S.size();
+    vector<vector<int> > res(N+1, vector<int>(26, N));
+    for (int i = N-1; i >= 0; --i) {
+        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
+        res[i][S[i]-'a'] = i;
+    }
+    return res;
+}
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<string, int>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y;
+    cin >> x >> y;
+    return C(x,y);
+}
+struct Edge {
+    int u, v;
+    ll w;
+    Edge(){}
+    Edge(int u, int v, ll w): u(u), v(v), w(w){}
+};
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(int& x, int y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<vector<int>> to(n);
+    map<pii, int> mp;
+    rep(i,n-1){
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        to[a].push_back(b);
+        to[b].push_back(a);
+        mp[pii(a,b)] = i;
+        mp[pii(b,a)] = i;
+    }
+
+    int k = 0;
+    rep(i,n){
+        int siz = to[i].size();
+        k = max(k, siz);
+    }
+
+    cout << k << endl;
+    vector<int> ans(n-1);
+
+    vector<int> visited(n);
+    auto dfs = [&](auto dfs, int v, int pre) -> void {
+        visited[v] = true;
+        int cnt = 1;
+        for(int u : to[v]){
+            if(visited[u]) continue;
+            if(cnt==pre) cnt++;
+            ans[mp[pii(v,u)]] = cnt;
+            dfs(dfs,u,cnt);
+            cnt++;
+        }
+    };
+    dfs(dfs,0, 0);
+
+    rep(i,n-1) cout << ans[i] << endl;
+}
