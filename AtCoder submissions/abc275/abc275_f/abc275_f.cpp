446c230459879d4d6e621f4c7b576befd25685ee
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-05-25 10:26:47 +0000

    [abc275] abc275_f

---
+++ AtCoder submissions/abc275/abc275_f/abc275_f.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+//#define int long long
+//typedef long long int;
+using ll = long long; using db = double;
+using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる。
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
+//auto nex = calc_next で取得すると楽。
+vector<vector<int> > calc_next(const string &S) {
+    int N = (int)S.size();
+    vector<vector<int> > res(N+1, vector<int>(26, N));
+    for (int i = N-1; i >= 0; --i) {
+        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
+        res[i][S[i]-'a'] = i;
+    }
+    return res;
+}
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<string, int>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y;
+    cin >> x >> y;
+    return C(x,y);
+}
+void chmax(int& x, int y) {x = max(x, y);} // change max
+void chmin(int& x, int y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+int dp[3005][3005][2];
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    rep(i,n) cin >> a[i];
+    rep(i,n+1)rep(j,m+1)rep(k,2) dp[i][j][k] = inf;
+    dp[0][0][0] = 0;
+
+    rep(i,n)rep(j,m+1)rep(k,2){
+        int now = dp[i][j][k];
+        int na = a[i];
+        int ni = i+1;
+        {
+            int nj = j+na;
+            if(nj<=m) chmin(dp[ni][nj][0],now);
+        }
+        {
+            int cost = k==0 ? 1 : 0;
+            chmin(dp[ni][j][1],now+cost);
+        }
+    }
+
+    repp(j,m){
+        int ans = min(dp[n][j][0],dp[n][j][1]);
+        if(ans==inf) ans = -1;
+        cout << ans << endl;
+    }
+}
