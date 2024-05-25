7eab0e2de8c658a847ea29f41e7158cbe65a0da7
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-05-25 11:47:29 +0000

    [abc224] abc224_e

---
+++ AtCoder submissions/abc224/abc224_e/abc224_e.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+//#define int long long
+//typedef long long int;
+using ll = long long; using db = double;
+using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる。
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
+//auto nex = calc_next で取得すると楽。
+vector<vector<int> > calc_next(const string &S) {
+    int N = (int)S.size();
+    vector<vector<int> > res(N+1, vector<int>(26, N));
+    for (int i = N-1; i >= 0; --i) {
+        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
+        res[i][S[i]-'a'] = i;
+    }
+    return res;
+}
+ll c2(ll n) {return n*(n-1) / 2;}
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<ll, pii>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y;
+    cin >> x >> y;
+    return C(x,y);
+}
+void chmax(int& x, int y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+
+int main(){
+    int h, w, n;
+    cin >> h >> w >> n;
+    map<int,vector<pii>> mp;
+    map<pii,int> cell;
+    rep(i,n){
+        int r, c, a;
+        cin >> r >> c >> a;
+        mp[-a].emplace_back(r,c);
+        cell[pii(r,c)] = i;
+    }
+
+    vector<int> ans(n);
+    map<int,int> mxr,mxc;
+    for(auto [_,ps] : mp){
+        for(auto [r,c] : ps){
+            int now = max(mxr[r],mxc[c]);
+            ans[cell[pii(r,c)]] = now;
+        }
+        for(auto [r,c] : ps){
+            int now = ans[cell[pii(r,c)]];
+            chmax(mxr[r],now+1);
+            chmax(mxc[c],now+1);
+        }
+    }
+
+    rep(i,n) cout << ans[i] << endl;
+
+}
