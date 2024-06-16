6f476fd4b523c8c1519a68cdcec05f814cee6962
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-06-16 14:49:33 +0000

    [abc358] abc358_e

---
+++ AtCoder submissions/abc358/abc358_e/abc358_e.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repb(i, n) for (int i = (n-1); i >= 0; i--)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mkp make_pair
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<ll, int>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y; cin >> x >> y;
+    return C(x,y);
+}
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+struct modinv {
+  int n; vector<mint> d;
+  modinv(): n(2), d({0,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(-d[mint::mod()%n]*(mint::mod()/n)), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} invs;
+struct modfact {
+  int n; vector<mint> d;
+  modfact(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*n), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} facts;
+struct modfactinv {
+  int n; vector<mint> d;
+  modfactinv(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*invs(n)), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} ifacts;
+mint comb(int n, int k) {
+  if (n < k || k < 0) return 0;
+  return facts(n)*ifacts(k)*ifacts(n-k);
+}
+
+int main(){
+    int n = 26;
+    int k;
+    cin >> k;
+    vector<int> c(n);
+    rep(i,n) cin >> c[i];
+
+    vector<mint> dp(k+1);
+    dp[0] = 1;
+
+    rep(i,n){
+        vector<mint> pre(k+1);
+        swap(pre,dp);
+        rep(j,k+1){
+            rep(a,c[i]+1){
+                int nj = j+a;
+                if(nj>k) break;
+                dp[nj] += pre[j]*comb(nj,a);
+            }
+        }
+    }
+
+    mint ans = 0;
+    rep(i,k) ans += dp[i+1];
+    cout << ans.val() << endl;
+}
