f6f7ae1c2482eb801a8e66553ff06e5c2af5d256
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-06-23 04:12:37 +0000

    [dp] dp_d

--- AtCoder submissions/dp/dp_d/dp_d.cpp
+++ AtCoder submissions/dp/dp_d/dp_d.cpp
@@ -1,48 +1,65 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <stack>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
-using ll = long long;
-using P = pair<int, int>;
-using MP = map<int, vector<int>>;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repb(i, n) for (int i = (n-1); i >= 0; i--)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mkp make_pair
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
 using mint = modint998244353;
-void chmax(ll& x, ll y) {x = max(x, y);} // change max
-void chmin(int& x, int y) {x = min(x, y);}
-const int di[] = {-1, 0, 1, 0};
-const int dj[] = {0, 1, 0, -1};
-const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const int inf = 1001001001;
-const ll INF = 1e18;
-ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
 ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
 ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<ll, int>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y; cin >> x >> y;
+    return C(x,y);
+}
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 //97~122(a~z),65~90(A~Z)
+
 
 int main(){
     int n, W;
     cin >> n >> W;
-    vector<int> w(n), v(n);
-    rep(i, n) cin >> w[i] >> v[i];
+    vector<int> w(n);
+    vector<ll> v(n);
+    rep(i,n) cin >> w[i] >> v[i];
 
-    ll dp[n+5][W+5];
-    rep(i, n+1)rep(j, W+1) dp[i][j] = 0;
-    rep(i, n)rep(j, W+1){
-        chmax(dp[i+1][j], dp[i][j]);
-        if(j+w[i]<=W) chmax(dp[i+1][j+w[i]], dp[i][j] + v[i]);
+    vector<ll> dp(W+1,0);
+    rep(i,n){
+        vector<ll> pre(W+1,0);
+        swap(pre,dp);
+        for(int j=W; j>=0; j--){
+            chmax(dp[j],pre[j]);
+            if(j+w[i]<W+1) chmax(dp[j+w[i]],pre[j]+v[i]);
+        }
+        //rep(i,W+1) cout << dp[i] << endl;
     }
 
     ll ans = 0;
-    rep(i, W+1) chmax(ans, dp[n][i]);
+    rep(i,W+1) chmax(ans,dp[i]);
     cout << ans << endl;
 }
