8681b0c9ad3dbb0ac99e9b4febe1ef40757833ee
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-06-23 16:18:34 +0000

    [dp] dp_k

--- AtCoder submissions/dp/dp_k/dp_k.cpp
+++ AtCoder submissions/dp/dp_k/dp_k.cpp
@@ -1,50 +1,65 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <stack>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
-using ll = long long;
-using P = pair<int, int>;
-using MP = map<int, vector<int>>;
-using mint = modint998244353;
-void chmax(int& x, int y) {x = max(x, y);} // change max
-void chmin(int& x, int y) {x = min(x, y);}
-const int di[] = {0, 1, 0, -1};
-const int dj[] = {1, 0, -1, 0};
-const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const int inf = 1001001001;
-const ll INF = 1e18;
-ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repb(i, n) for (int i = (n-1); i >= 0; i--)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mkp make_pair
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+//using mint = modint998244353;
+using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
 ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
 ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<ll, int>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y; cin >> x >> y;
+    return C(x,y);
+}
+void chmax(int& x, int y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 //97~122(a~z),65~90(A~Z)
 
-bool dp[100100];
 
 int main(){
     int n, k;
     cin >> n >> k;
     vector<int> a(n);
-    rep(i, n) cin >> a[i];
+    rep(i,n) cin >> a[i];
 
-    rep(i, k+1) dp[i] = false;
+    sort(a.begin(),a.end());
 
-    repp(i, k){
-        rep(j, n){
-            if(i-a[j]>=0) dp[i] |= !dp[i-a[j]];
+    vector<bool> dp(k+1);
+    dp[0] = false;
+    repp(i,k){
+        bool ok = false;
+        rep(j,n){
+            if(i-a[j]<0) continue;
+            if(dp[i-a[j]]==false) ok = true;
         }
+        dp[i] = ok;
     }
 
-    if(dp[k])cout << "First" << endl;
+    if(dp[k]) cout << "First" << endl;
     else cout << "Second" << endl;
 }
