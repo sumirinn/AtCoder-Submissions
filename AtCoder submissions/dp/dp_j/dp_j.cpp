30704aad75db00ba1e456e5ab2dbb903913ef211
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-06-23 08:15:10 +0000

    [dp] dp_j

---
+++ AtCoder submissions/dp/dp_j/dp_j.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repb(i, n) for (int i = (n-1); i >= 0; i--)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
+#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+#define pb push_back
+#define eb emplace_back
+#define mkp make_pair
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+//using mint = modint998244353;
+using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<ll, int>;
+using MP = map<int, vector<int>>;
+using C = complex<double>;
+C inC(){
+    double x, y; cin >> x >> y;
+    return C(x,y);
+}
+void chmax(int& x, int y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+const int nmx = 305;
+double dp[nmx][nmx][nmx];
+
+int main(){
+    int n;
+    cin >> n;
+    const int m = 3;
+    vector<int> a(m);
+    rep(i,n){
+        int b;
+        cin >> b;
+        b--;
+        a[b]++;
+    }
+
+    rep(k,n+1)rep(j,n+1)rep(i,n+1){
+        int s = i+j+k;
+        if(s==0) continue;
+        dp[i][j][k] = (double)n;
+        if(i) dp[i][j][k] += dp[i-1][j][k]*i;
+        if(j) dp[i][j][k] += dp[i+1][j-1][k]*j;
+        if(k) dp[i][j][k] += dp[i][j+1][k-1]*k;
+        dp[i][j][k] /= s;
+
+    }
+    cout << setprecision(20) << fixed << dp[a[0]][a[1]][a[2]] << endl;
+}
