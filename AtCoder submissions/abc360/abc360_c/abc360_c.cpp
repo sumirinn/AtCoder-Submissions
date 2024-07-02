01a1d7a8e4e5ab4fb43dcc6cfc960ea28c64a136
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-07-02 09:06:16 +0000

    [abc360] abc360_c

---
+++ AtCoder submissions/abc360/abc360_c/abc360_c.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<ll, int>;
+using C = complex<double>;
+C inC(){
+    double x, y; cin >> x >> y;
+    return C(x,y);
+}
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n), w(n);
+    rep(i,n) cin >> a[i];
+    rep(i,n) a[i]--;
+    rep(i,n) cin >> w[i];
+
+    vector<int> b(n);
+    int ans = 0;
+    rep(i,n){
+        if(b[a[i]]==0) b[a[i]] = w[i];
+        else{
+            int c = min(b[a[i]],w[i]);
+            int nex = max(b[a[i]],w[i]);
+            ans += c;
+            b[a[i]] = nex;
+        }
+    }
+
+    cout << ans << endl;
+}
