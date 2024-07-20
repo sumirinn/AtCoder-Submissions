e11119e6849fbaff31929b10d376f935348a789e
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-07-20 10:58:40 +0000

    [abc156] abc156_e

---
+++ AtCoder submissions/abc156/abc156_e/abc156_e.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+//using mint = modint998244353;
+using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<ll, int>;
+using C = complex<double>;
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+// combination mod prime
+// https://youtu.be/8uowVvQ_-Mo?t=6002
+// https://youtu.be/Tgd_zLfRZOQ?t=9928
+//逆元を計算するための構造体
+struct modinv {
+    //nは現在計算している逆元の最大の数を示す整数
+    //d は逆元を保存するためのvector
+    int n; vector<mint> d;
+    modinv(): n(2), d({0,1}) {}
+    mint operator()(int i) {
+        while (n <= i) d.push_back(-d[mint::mod()%n]*(mint::mod()/n)), ++n;
+        return d[i];
+    }
+    mint operator[](int i) const { return d[i];}
+} invs;
+//整数の階乗を計算するための構造体
+struct modfact {
+    int n; vector<mint> d;
+    modfact(): n(2), d({1,1}) {}
+    mint operator()(int i) {
+        while (n <= i) d.push_back(d.back()*n), ++n;
+        return d[i];
+    }
+    mint operator[](int i) const { return d[i];}
+} facts;
+//階乗の逆元を計算するための構造体
+struct modfactinv {
+    int n; vector<mint> d;
+    modfactinv(): n(2), d({1,1}) {}
+    mint operator()(int i) {
+        while (n <= i) d.push_back(d.back()*invs(n)), ++n;
+        return d[i];
+    }
+    mint operator[](int i) const { return d[i];}
+} ifacts;
+mint comb(int n, int k) {
+    if (n < k || k < 0) return 0;
+    return facts(n)*ifacts(k)*ifacts(n-k);
+}
+
+
+int main(){
+    ll n, k;
+    cin >> n >> k;
+    chmin(k,n-1);
+
+    mint ans = 0;
+    rep(i,k+1) ans += comb(n,i)*comb((n-i-1)+i,i);
+    
+    cout << ans.val() << endl;
+}
