f017ddd25367f33f8285e3b5e1c194fea9fd14a7
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-07-20 10:39:40 +0000

    [abc157] abc157_e

---
+++ AtCoder submissions/abc157/abc157_e/abc157_e.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>;  
+using pdd = pair<double, double>; using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+const int inf = 1001001001; const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<ll, int>;
+using C = complex<double>;
+void chmax(ll& x, ll y) {x = max(x, y);} // change max
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//97~122(a~z),65~90(A~Z)
+
+
+int op(int a, int b){
+    int res = 0;
+    rep(i,26) if(((1<<i)&a)||((1<<i)&b)) res += 1<<i;
+    return res;
+}
+
+int e(){return 0;}
+
+int main(){
+    int n, q;
+    string s;
+    cin >> n >> s >> q;
+
+    segtree<int,op,e> st(n);
+    rep(i,n) st.set(i,1<<(int(s[i]-'a')));
+
+    vector<int> ans;
+    rep(qi,q){
+        int t;
+        cin >> t;
+        if(t==1){
+            int i;
+            char c;
+            cin >> i >> c;
+            i--;
+            st.set(i,1<<(int(c-'a')));
+        }
+        if(t==2){
+            int l, r;
+            cin >> l >> r;
+            l--;
+            ans.pb(__builtin_popcount(st.prod(l,r)));
+        }
+    }
+
+    for(int res : ans) cout << res << endl;
+}
