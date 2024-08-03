82a144fca05bd6a2fd9a58df83f43f17173e11f0
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-08-03 09:37:34 +0000

    [abc364] abc364_d

---
+++ AtCoder submissions/abc364/abc364_d/abc364_d.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<double, double>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<int, pii>;
+using C = complex<double>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    rep(i,n) cin >> a[i];
+    sort(a.begin(),a.end());
+
+    vector<int> ans(q);
+    rep(qi,q){
+        int b, k;
+        cin >> b >> k;
+
+        auto f =[&](int wj)->bool{
+            int l=b-wj, r=b+wj;
+            int li = lower_bound(a.begin(),a.end(),l) - a.begin();
+            int ri = lower_bound(a.begin(),a.end(),r+1) - a.begin();
+            return ri-li >= k;
+        };
+
+        int ac = 2e8, wa = -1;
+        while(wa+1<ac){
+            int wj = (wa+ac)/2;
+            if(f(wj)) ac = wj;
+            else wa = wj;
+        }
+        ans[qi] = ac;
+    }
+
+    rep(i,q) cout << ans[i] << endl;
+}
