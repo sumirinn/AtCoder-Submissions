4ec8f95135333509479d8bf43c790c003e54b103
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-08-08 03:43:44 +0000

    [typical90] typical90_d

--- AtCoder submissions/typical90/typical90_d/typical90_d.cpp
+++ AtCoder submissions/typical90/typical90_d/typical90_d.cpp
@@ -1,49 +1,51 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <stack>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
-using ll = long long;
-using MP = map<int, vector<int>>;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<double, double>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 3e18;
 using mint = modint998244353;
-using P = pair<int, int>;
-void chmax(ll& x, ll y) {x = max(x, y);} // change max
-void chmin(ll& x, ll y) {x = min(x, y);}
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<int, pii>;
+using C = complex<double>;
+void chmax(int& x, int y) {x = max(x, y);} 
+void chmin(int& x, int y) {x = min(x, y);}
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
 const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const int inf = 1001001001;
-const ll INF = 1e18;
-ll c2(ll n) {return n*(n-1) / 2;}
-ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
-ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-//97~122(a~z),65~90(A~Z)
 
 
 
 int main(){
-    int h, w; cin >> h >> w;
-    vector<vector<int>> a(h, vector<int>(w));
-    rep(i,h)rep(j,w) cin >> a[i][j];
-    
-    vector<int> u(h), v(w);
+    int h, w;
+    cin >> h >> w;
+    vector<vector<int>> a(h,vector<int>(w));
+    vector<int> H(h,0), W(w,0);
     rep(i,h)rep(j,w){
-        u[i] += a[i][j];
-        v[j] += a[i][j];
+        cin >> a[i][j];
+        H[i] += a[i][j];
+        W[j] += a[i][j];
     }
+
     rep(i,h){
-        rep(j,w) cout << u[i] + v[j] - a[i][j] << " ";
+        rep(j,w) cout << H[i] + W[j] - a[i][j] << " ";
         cout << endl;
     }
 }
