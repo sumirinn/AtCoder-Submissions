0733800919b8f9ccd87acc7b919fcbf33821b1b6
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-08-08 11:41:10 +0000

    [typical90] typical90_h

--- AtCoder submissions/typical90/typical90_h/typical90_h.cpp
+++ AtCoder submissions/typical90/typical90_h/typical90_h.cpp
@@ -1,52 +1,53 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <stack>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
-using ll = long long;
-using MP = map<int, vector<int>>;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<double, double>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 3e18;
 //using mint = modint998244353;
 using mint = modint1000000007;
-using P = pair<int, int>;
-void chmax(ll& x, ll y) {x = max(x, y);} // change max
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<int, pii>;
+using C = complex<double>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
 void chmin(ll& x, ll y) {x = min(x, y);}
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
 const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const int inf = 1001001001;
-const ll INF = 1e18;
-ll c2(ll n) {return n*(n-1) / 2;}
-ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
-ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
-//auto nex = calc_next で取得すると楽。
-vector<vector<int> > calc_next(const string &S) {
-    int N = (int)S.size();
-    vector<vector<int> > res(N+1, vector<int>(26, N));
-    for (int i = N-1; i >= 0; --i) {
-        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
-        res[i][S[i]-'a'] = i;
-    }
-    return res;
-}
-//97~122(a~z),65~90(A~Z)
+
+
 
 int main(){
-    int n; string s; cin >> n >> s;
+    int n;
+    string s;
+    cin >> n >> s;
 
-    string t = "atcoder"; int m = t.size();
-    vector<mint> dp(m+1, 0); dp[0] = 1;
-    rep(i,n)rep(j,m)if(s[i]==t[m-j-1]) dp[m-j] += dp[m-j-1];
-    cout << dp[m].val() << endl;
+    string t = "atcoder";
+    const int m = t.size();
+
+    vector<mint> dp(m,0);
+    rep(i,n){
+        for(int j=m-1; j>=0; j--){
+            if(j==0 && s[i]==t[j]) dp[j]++;
+            else if(s[i]==t[j]) dp[j] += dp[j-1];
+        }
+    }
+
+    cout << dp[m-1].val() << endl;
 }
