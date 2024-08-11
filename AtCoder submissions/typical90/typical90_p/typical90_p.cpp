fcc5514c1925fa8322068cf3b496d62277f13fda
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-08-11 12:34:07 +0000

    [typical90] typical90_p

---
+++ AtCoder submissions/typical90/typical90_p/typical90_p.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<double, double>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+//using mint = modint998244353;
+using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<double, int>;
+using C = complex<double>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+
+int main(){
+    ll n;
+    cin >> n;
+    int m = 3;
+    vector<ll> a(m);
+    rep(i,m) cin >> a[i];
+    
+    ll ans = INF;
+    const ll mx = 1e4;
+    for(ll i=0; i<mx; i++)for(ll j=0; j<mx; j++){
+        ll res = n - a[0]*i - a[1]*j;
+        if(res<0) continue;
+        if(res%a[2]==0){
+            ll now = i + j + res/a[2];
+            ans = min(ans,now);
+        }
+    }
+
+    cout << ans << endl;
+}
