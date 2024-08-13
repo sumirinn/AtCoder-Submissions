62173edf2e59292d70d6b6af26ddd3380c03394f
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-08-13 22:50:36 +0000

    [abc366] abc366_f

---
+++ AtCoder submissions/abc366/abc366_f/abc366_f.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<double, double>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+//using mint = modint998244353;
+using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using P = pair<double, int>;
+using C = complex<double>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+
+struct F{
+    ll a, b;
+    F(ll a=0, ll b=0):a(a),b(b) {};
+    ll operator()(ll x){return a*x+b;}
+};
+
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<F> f(n);
+    rep(i,n) cin >> f[i].a >> f[i].b;
+    sort(f.begin(),f.end(),[&](F f1, F f2){
+        return f2(f1(0)) > f1(f2(0));
+    });
+
+    vector<ll> dp(k+1,1);
+    rep(i,n){
+        for(int j=k-1; j>=0; j--){
+            chmax(dp[j+1],f[i](dp[j]));
+        }
+    }
+
+    cout << dp[k] << endl;
+}
