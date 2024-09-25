61efa055a586c5bf5a4d7d5782abc994cd193dda
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-09-25 04:38:41 +0000

    [abc372] abc372_c

---
+++ AtCoder submissions/abc372/abc372_c/abc372_c.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<double, double>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<double, int>;
+using C = complex<double>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    string s;
+    cin >> s;
+
+    int ans = 0;
+    rep(i,n-2){
+        if(s[i]=='A' && s[i+1]=='B' && s[i+2]=='C') ans++;
+    }
+
+    rep(qi,q){
+        int x;
+        char c;
+        cin >> x >> c;
+        x--;
+
+        for(int i=x-2; i<=x+2; i++){
+            if(i<0 || n<i) continue;
+            if(s[i]=='A' && s[i+1]=='B' && s[i+2]=='C') ans--;
+        }
+        s[x] = c;
+        for(int i=x-2; i<=x+2; i++){
+            if(i<0 || n<i) continue;
+            if(s[i]=='A' && s[i+1]=='B' && s[i+2]=='C') ans++;
+        }
+
+        cout << ans << endl;
+    }
+}
