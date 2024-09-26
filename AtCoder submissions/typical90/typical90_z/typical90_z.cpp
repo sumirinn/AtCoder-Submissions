af6ccc8bbc087ec0f5b6f8d6e73820a06d7b162b
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-09-26 04:04:20 +0000

    [typical90] typical90_z

---
+++ AtCoder submissions/typical90/typical90_z/typical90_z.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<double, double>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<double, int>;
+using C = complex<double>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<vector<int>> to(n);
+    rep(i,n-1){
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        to[u].pb(v);
+        to[v].pb(u);
+    }
+
+    set<int> st;
+    auto dfs =[&](auto dfs, int now, int pre, bool pre_c)->void{
+        bool now_c = !pre_c;
+        if(now_c) st.insert(now);
+        for(int nex : to[now]){
+            if(nex==pre) continue;
+            dfs(dfs,nex,now,now_c);
+        }
+    };
+    dfs(dfs,0,-1,false);
+
+    if(st.size()<n/2){
+        set<int> st2;
+        swap(st2,st);
+        for(int i=0; i<n; i++) if(!st2.count(i)) st.insert(i);
+    }
+
+    int cnt = n/2;
+    for(int e : st){
+        cout << e + 1 << " ";
+        cnt--;
+        if(cnt==0) break;
+    }
+    cout << endl;
+}
