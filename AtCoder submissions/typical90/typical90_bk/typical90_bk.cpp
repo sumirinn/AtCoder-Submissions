c5fbc0d645a9d4ebc8c3ca9ef0b16e161912a7a1
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-09-28 07:22:00 +0000

    [typical90] typical90_bk

---
+++ AtCoder submissions/typical90/typical90_bk/typical90_bk.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 1e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<db, int>;
+using C = complex<db>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+
+int main(){
+    int h, w;
+    cin >> h >> w;
+    vector<vector<int>> p(h,vector<int>(w));
+    rep(i,h)rep(j,w) cin >> p[i][j];
+
+    int h2=1<<h, hw=h*w;
+    int ans = 0;
+    rep(s,h2){
+        if(s==0) continue;
+        vector<int> cnt(hw,0);
+        rep(j,w){
+            int num = -1;
+            rep(i,h){
+                if(s&(1<<i)){
+                    if(num==-1) num = p[i][j];
+                    else if(num!=p[i][j]) num=inf;
+                }
+            }
+            if(num==inf) continue;
+            cnt[num-1]++;
+        }
+        int b = __builtin_popcount(s);
+        rep(i,hw) ans = max(ans, b*cnt[i]);
+    }
+
+    cout << ans << endl;
+}
