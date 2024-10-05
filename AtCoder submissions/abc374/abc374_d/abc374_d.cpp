c64b0a99b64f4ad71c8fd82d9cd4c821005c084e
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-10-05 12:25:02 +0000

    [abc374] abc374_d

---
+++ AtCoder submissions/abc374/abc374_d/abc374_d.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 1e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<db, int>;
+using C = complex<db>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+
+int main(){
+    int n;
+    db s, t;
+    cin >> n >> s >> t;
+    vector<pdd> f(n), g(n);
+    rep(i,n){
+        cin >> f[i].fi >> f[i].se;
+        cin >> g[i].fi >> g[i].se; 
+    }
+
+    vector<int> p(n);
+    rep(i,n) p[i] = i;
+    
+    auto dist =[&](pdd st, pdd gl)->db{
+        db x = st.fi - gl.fi;
+        db y = st.se - gl.se;
+        return sqrt(x*x+y*y);
+    };
+
+    db ans = inf;
+    do{
+        rep(msk,1<<n){
+            pdd now = pdd(0,0);
+            db cnt = 0;
+            rep(i,n){
+                int ni = p[i];
+                if(msk>>i&1){
+                    db nc = dist(now,f[ni]);
+                    cnt += nc / s;
+                    now = f[ni];
+                    db c = dist(now,g[ni]);
+                    cnt += c / t;
+                    now = g[ni];
+                }
+                else{
+                    db nc = dist(now,g[ni]);
+                    cnt += nc / s;
+                    now = g[ni];
+                    db c = dist(now,f[ni]);
+                    cnt += c / t;
+                    now = f[ni];
+                }
+            }
+            ans = min(ans,cnt);
+        }
+    }while(next_permutation(p.begin(),p.end()));
+
+    cout << fixed << setprecision(20) << ans << endl;
+}
