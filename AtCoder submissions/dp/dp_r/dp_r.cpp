165a89e95822c948c91d3ddb51a73af011517548
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-10-15 22:31:15 +0000

    [dp] dp_r

--- AtCoder submissions/dp/dp_r/dp_r.cpp
+++ AtCoder submissions/dp/dp_r/dp_r.cpp
@@ -3,20 +3,16 @@ using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (n); i++)
-#define repb(i, n) for (int i = (n-1); i >= 0; i--)
 #define repp(i, n) for (int i = 1; i <= (n); i++)
-#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
-#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
 #define pb push_back
 #define eb emplace_back
-#define mkp make_pair
 #define fi first
 #define se second
 using ll = long long; using db = double; using ull = unsigned long long;
-using pii = pair<int, int>; using pll = pair<ll, ll>;  
-using pdd = pair<double, double>; using pli = pair<ll, int>;
-using pil = pair<int, ll>;
-const int inf = 1001001001; const ll INF = 3e18;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 1e18;
 //using mint = modint998244353;
 using mint = modint1000000007;
 //using mint = modint;
@@ -24,61 +20,48 @@ using mint = modint1000000007;
 //a,bが0だと使えないことに注意
 ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
 ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-ll c2(ll n) {return n*(n-1) / 2;} ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-using P = pair<ll, int>;
-using MP = map<int, vector<int>>;
-using C = complex<double>;
-C inC(){
-    double x, y; cin >> x >> y;
-    return C(x,y);
-}
-void chmax(ll& x, ll y) {x = max(x, y);} // change max
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<db, int>;
+using C = complex<db>;
+void chmax(int& x, int y) {x = max(x, y);} 
 void chmin(ll& x, ll y) {x = min(x, y);}
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
-const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-//97~122(a~z),65~90(A~Z)
-
-const int m = 60;
-const int nmax = 50;
-mint A[m+1][nmax][nmax];
+const int dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
+const int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
 
 int main(){
-    int n; ll k;
+    int n;
+    ll k;
     cin >> n >> k;
+    int d = 60;
+    using vvm = vector<vector<mint>>;
+    vector<vvm> a(d+1,vvm(n,vector<mint>(n,0)));
     rep(i,n)rep(j,n){
         int x;
         cin >> x;
-        A[0][i][j] = x;
+        a[0][i][j] = x;
     }
 
-    repp(s,m){
-        rep(i,n){
-            rep(j,n){
-                A[s][i][j] = 0;
-                rep(k,n) A[s][i][j] += A[s-1][i][k]*A[s-1][k][j];
-            }
+    rep(s,d){
+        rep(i,n)rep(j,n)rep(k,n){
+            a[s+1][i][j] += a[s][i][k]*a[s][k][j];
         }
     }
 
-
-    vector<vector<mint>> dp(n,vector<mint>(n));
-    rep(i,n) dp[i][i] = 1;
-    rep(s,m){
-        if(k&(ll)1<<s){
-            //cout << s << endl;
-            vector<vector<mint>> nex(n,vector<mint>(n));
-            rep(i,n)rep(j,n){
-                rep(k,n) nex[i][j] += dp[i][k]*A[s][k][j];
-            }
-            swap(nex,dp);
+    vvm ans(n,vector<mint>(n,0));
+    rep(i,n) ans[i][i] = 1;
+    rep(s,d+1)if(k>>s&1){
+        vvm pre(n,vector<mint>(n,0));
+        swap(pre,ans);
+        rep(i,n)rep(j,n)rep(k,n){
+            ans[i][j] += pre[i][k]*a[s][k][j];
         }
     }
 
-
-    mint ans = 0;
-    rep(i,n)rep(j,n) ans += dp[i][j];
-    cout << ans.val() << endl;
+    mint answer = 0;
+    rep(i,n)rep(j,n) answer += ans[i][j];
+    cout << answer.val() << endl;
 }
