08d3255c9c959b7f4e287af915ca1a7419835b3c
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-10-15 08:08:35 +0000

    [dp] dp_a

--- AtCoder submissions/dp/dp_a/dp_a.cpp
+++ AtCoder submissions/dp/dp_a/dp_a.cpp
@@ -3,20 +3,16 @@ using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (n); i++)
-#define repb(i, n) for (int i = (n-1); i >= 0; i--)
 #define repp(i, n) for (int i = 1; i <= (n); i++)
-#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
-#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
 #define pb push_back
 #define eb emplace_back
-#define mkp make_pair
 #define fi first
 #define se second
 using ll = long long; using db = double; using ull = unsigned long long;
-using pii = pair<int, int>; using pll = pair<ll, ll>;  
-using pdd = pair<double, double>; using pli = pair<ll, int>;
-using pil = pair<int, ll>;
-const int inf = 1001001001; const ll INF = 3e18;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 1e18;
 using mint = modint998244353;
 //using mint = modint1000000007;
 //using mint = modint;
@@ -24,36 +20,28 @@ using mint = modint998244353;
 //a,bが0だと使えないことに注意
 ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
 ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-ll c2(ll n) {return n*(n-1) / 2;} ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-using P = pair<ll, int>;
-using MP = map<int, vector<int>>;
-using C = complex<double>;
-C inC(){
-    double x, y; cin >> x >> y;
-    return C(x,y);
-}
-void chmax(ll& x, ll y) {x = max(x, y);} // change max
-void chmin(ll& x, ll y) {x = min(x, y);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<db, int>;
+using C = complex<db>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(int& x, int y) {x = min(x, y);}
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
-const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-//97~122(a~z),65~90(A~Z)
-
+const int dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
+const int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
 
 int main(){
     int n;
     cin >> n;
-    vector<ll> h(n);
+    vector<int> h(n);
     rep(i,n) cin >> h[i];
-
-    vector<ll> dp(n,INF);
+    vector<int> dp(n,inf);
     dp[0] = 0;
     rep(i,n){
-        if(i+1<n) chmin(dp[i+1],dp[i]+abs(h[i+1]-h[i]));
-        if(i+2<n) chmin(dp[i+2],dp[i]+abs(h[i+2]-h[i]));
+        if(i<n-1) chmin(dp[i+1], dp[i] + abs(h[i+1]-h[i]));
+        if(i<n-2) chmin(dp[i+2], dp[i] + abs(h[i+2]-h[i]));
     }
-
     cout << dp[n-1] << endl;
 }
