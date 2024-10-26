9461a3be3fe48c8371e052ac72470bbf197d0968
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-10-26 08:28:21 +0000

    [abc354] abc354_e

--- AtCoder submissions/abc354/abc354_e/abc354_e.cpp
+++ AtCoder submissions/abc354/abc354_e/abc354_e.cpp
@@ -4,94 +4,61 @@ using namespace std;
 using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define repp(i, n) for (int i = 1; i <= (n); i++)
-#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
-#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
 #define pb push_back
 #define eb emplace_back
-#define mp make_pair
 #define fi first
 #define se second
-using ll = long long; using db = double;
-using ull = unsigned long long;
-using pii = pair<int, int>; using pll = pair<ll, ll>;  
-using pdd = pair<double, double>; using pli = pair<ll, int>;
-using pil = pair<int, ll>;
-const int inf = 1001001001; const ll INF = 3e18;
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 1e18;
 using mint = modint998244353;
 //using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
 //a,bが0だと使えないことに注意
 ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
 ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
-//auto nex = calc_next で取得すると楽。
-vector<vector<int> > calc_next(const string &S) {
-    int N = (int)S.size();
-    vector<vector<int> > res(N+1, vector<int>(26, N));
-    for (int i = N-1; i >= 0; --i) {
-        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
-        res[i][S[i]-'a'] = i;
-    }
-    return res;
-}
-ll c2(ll n) {return n*(n-1) / 2;}
+ll c2(ll n) {return n*(n-1) / 2;} 
 ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-using P = pair<string, int>;
-using MP = map<int, vector<int>>;
-using C = complex<double>;
-C inC(){
-    double x, y;
-    cin >> x >> y;
-    return C(x,y);
-}
-struct Edge {
-    int u, v;
-    ll w;
-    Edge(){}
-    Edge(int u, int v, ll w): u(u), v(v), w(w){}
-};
-void chmax(ll& x, ll y) {x = max(x, y);} // change max
+//using P = pair<db, int>;
+using C = complex<db>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
 void chmin(int& x, int y) {x = min(x, y);}
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
 const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-//97~122(a~z),65~90(A~Z)
-
-ll f(ll a, ll b) {
-    ll ra = a%4; a -= ra;
-    ll rb = b%2; b -= rb;
-    ll res = a*b;
-    if(rb) res += a;
-    if(ra>=1){
-        res += b*3/2;
-        if(rb) res += 2;
-    }
-    if(ra>=2){
-        res += b*3/2;
-        if(rb) res += 1;
-    }
-    if(ra>=3) res += b/2;
-    return res;
-}
 
 
 int main(){
     int n;
     cin >> n;
-    vector<int> a(n), b(n);
+    vector<ll> a(n), b(n);
     rep(i,n) cin >> a[i] >> b[i];
-    int n2 = 1<<n;
-    vector<bool> dp(n2);
-    rep(s,n2){
-        bool now = false;
-        rep(i,n)rep(j,i)if(s>>i&1)if(s>>j&1){
-            if(a[i]==a[j] || b[i]==b[j]){
-                if(!dp[s^1<<i^1<<j]) now = true;
-            }
-        }
-        dp[s] = now;
-    }
 
-    if(dp[n2-1]) cout << "Takahashi" << endl;
-    else cout << "Aoki" << endl;
+    int n2 = 1<<n;
+    vector<int> mem(n2,inf);
+    auto dfs =[&](auto dfs, int now)->bool{
+        if(mem[now]!=inf) return mem[now];
+        bool res = false;
+        rep(i,n)rep(j,n){
+            if(i==j) continue;
+            if((1&(now>>i))==0 || (1&(now>>j))==0) continue;
+            if(a[i]==a[j] || b[i]==b[j]){
+                int nex = now;
+                nex -= 1<<i;
+                nex -= 1<<j;
+                if(dfs(dfs,nex)==false) res = true; 
+            } 
+        }
+        return mem[now] = res;
+    };
+
+    int st = 0;
+    rep(i,n) st |= 1<<i;
+    dfs(dfs,st);
+    if(mem[st]) cout << "Takahashi" << endl;
+    else cout << "Aoki" << endl; 
 }
