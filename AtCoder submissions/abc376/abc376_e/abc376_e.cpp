0db2566bed2e5aba6d59e148df42d98a72d62282
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-10-27 12:10:18 +0000

    [abc376] abc376_e

---
+++ AtCoder submissions/abc376/abc376_e/abc376_e.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 1e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<db, int>;
+using C = complex<db>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(int& x, int y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vector<pll> ab(n);
+    rep(i,n) cin >> ab[i].fi;
+    rep(i,n) cin >> ab[i].se;
+    sort(ab.begin(),ab.end());
+
+    priority_queue<ll> q;
+    ll ans = INF;
+    ll sumb = 0;
+    for(auto[a,b] : ab){
+        if(q.size()==k-1) ans = min(ans,(sumb+b)*a);
+        sumb += b;
+        q.push(b);
+        if(q.size()==k){
+            sumb -= q.top();
+            q.pop();
+        }
+    }
+    cout << ans << endl;
+}
+
+
+int main(){
+    int t;
+    cin >> t;
+    rep(ti,t) solve();
+}
