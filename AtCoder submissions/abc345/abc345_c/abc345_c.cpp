de1068ea4863b57525d02ff35e396cc6f5783371
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-11-02 10:33:37 +0000

    [abc345] abc345_c

--- AtCoder submissions/abc345/abc345_c/abc345_c.cpp
+++ AtCoder submissions/abc345/abc345_c/abc345_c.cpp
@@ -1,47 +1,50 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <stack>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
-using ll = long long;
-using MP = map<int, vector<int>>;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 1e18;
 using mint = modint998244353;
-using P = pair<ll, ll>;
-void chmax(ll& x, ll y) {x = max(x, y);} // change max
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<db, int>;
+using C = complex<db>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
 void chmin(ll& x, ll y) {x = min(x, y);}
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
 const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const int inf = 1001001001;
-const ll INF = 1e18;
-ll c2(ll n) {return n*(n-1) / 2;}
-ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
-ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-//97~122(a~z),65~90(A~Z)
+
 
 int main(){
     string s;
     cin >> s;
+
     ll n = s.size();
-    ll n2 = c2(n);
-    map<char, ll> mp;
-    rep(i,n) mp[s[i]]++;
-    bool ok = false;
-    for(auto[c, cnt] : mp){
-        n2 -= c2(cnt);
-        if(cnt>=2) ok = true;
+    const int m = 26;
+    vector<ll> cnt(m);
+    rep(i,n) cnt[s[i]-'a']++;
+    ll ans = c2(n);
+    rep(i,m)if(cnt[i]>=2){
+        ans++;
+        break;
     }
-    if(ok) n2++;
-    cout << n2 << endl;
+    rep(i,m) ans -= c2(cnt[i]);
+    cout << ans << endl;
 }
