e35c9e3dcc1370f6476c2c3b8161d63731c3c338
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-11-05 17:39:17 +0000

    [abc377] abc377_g

---
+++ AtCoder submissions/abc377/abc377_g/abc377_g.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
+const ll INF = 1e18;
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<db, int>;
+using C = complex<db>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+
+/* Trie 木： 文字の種類(char_size)、int型で0に対応する文字(base)
+    insert(word): 単語 word を Trie 木に挿入する
+    search(word): 単語 word が Trie 木にあるか判定する
+    start_with(prefix):  prefix が一致する単語が Trie 木にあるか判定する
+    count(): 挿入した単語の数を返す
+    size(): Trie 木の頂点数を返す
+    計算量：insert, search ともに O(M)（Mは単語の長さ）
+*/
+template <int char_size, int base>
+struct Trie {
+    struct Node {            // 頂点を表す構造体
+        vector<int> next;    // 子の頂点番号を格納。存在しなければ-1
+        vector<int> accept;  // 末端がこの頂点になる単語の word_id を保存
+        int c;               // base からの間隔をint型で表現したもの
+        int common;          // いくつの単語がこの頂点を共有しているか
+        Node(int c_) : c(c_), common(0) {
+            next.assign(char_size, -1);
+        }
+    };
+    vector<Node> nodes;  // trie 木本体
+    vector<int> nearest = vector<int>(200000+5,inf);
+    int root;
+    Trie() : root(0) {
+        nodes.push_back(Node(root));
+    }
+    // 単語の挿入
+    int insert(const string &word, int word_id) {
+        int res = word.size();
+        int node_id = 0;
+        for (int i = 0; i < (int)word.size(); i++) {
+            int c = (int)(word[i] - base);
+            int &next_id = nodes[node_id].next[c];
+            if (next_id == -1) {  // 次の頂点が存在しなければ追加
+                next_id = (int)nodes.size();
+                nodes.push_back(Node(c));
+                //nearest.pb(inf);
+            }
+            ++nodes[node_id].common;
+            node_id = next_id;
+            int dist = word.size()-i-1;
+            res = min(res,nearest[node_id]+dist);
+            nearest[node_id] = min(nearest[node_id],dist);
+        }
+        ++nodes[node_id].common;
+        nodes[node_id].accept.push_back(word_id);
+        
+        return res;
+    }
+    int insert(const string &word) {
+        int res = insert(word, nodes[0].common);
+        return res;
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+
+    Trie<26,'a'> t;
+    rep(i,n){
+        string s;
+        cin >> s;
+        int ans = t.insert(s);
+        cout << ans << endl;
+    }
+}
