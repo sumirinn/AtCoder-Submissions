35077ec1776b192aab02488dbf950e4ecbeca6f1
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-11-06 07:51:20 +0000

    [abc342] abc342_c

--- AtCoder submissions/abc342/abc342_c/abc342_c.cpp
+++ AtCoder submissions/abc342/abc342_c/abc342_c.cpp
@@ -1,52 +1,53 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <stack>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
-using ll = long long;
-using P = pair<int, int>;
-using MP = map<int, vector<int>>;
-using mint = modint998244353;
-void chmax(ll& x, ll y) {x = max(x, y);} // change max
-void chmin(ll& x, ll y) {x = min(x, y);}
-const int di[] = {0, 1, 0, -1};
-const int dj[] = {1, 0, -1, 0};
-const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const int inf = 1001001001;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repp(i, n) for (int i = 1; i <= (n); i++)
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+using ll = long long; using db = double; using ull = unsigned long long;
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+const int inf = 1001001001; 
 const ll INF = 1e18;
-ll c2(ll n) {return n*(n-1) / 2;}
-ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;
+//mint::set_mod(m);で定義できる
+//a,bが0だと使えないことに注意
 ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
 ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-//97~122(a~z),65~90(A~Z)
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+//using P = pair<db, int>;
+using C = complex<db>;
+void chmax(ll& x, ll y) {x = max(x, y);} 
+void chmin(ll& x, ll y) {x = min(x, y);}
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
 
 int main(){
-    int n;
+    int n, q;
     string s;
-    cin >> n >> s;
+    cin >> n >> s >> q;
 
-    vector<int> v(26);
-    rep(i, 26) v[i] = i;
-    int q;
-    cin >> q;
-    rep(qi, q){
+    const int m = 26;
+    vector<int> change(m);
+    rep(i,m) change[i] = i;
+    rep(qi,q){
         char c, d;
         cin >> c >> d;
-        rep(i, 26){
-            if(v[i]==int(c)-97) v[i] = int(d)-97;
-        }
+        int C = c - 'a';
+        int D = d - 'a';
+        rep(j,m) if(change[j]==C) change[j] = D;
     }
 
-    rep(i, n) cout << char(v[int(s[i])-97]+97);
+    rep(i,n) cout << char(change[s[i]-'a']+'a');
     cout << endl;
 }
