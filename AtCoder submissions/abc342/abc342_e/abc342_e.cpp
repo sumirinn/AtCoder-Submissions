4d2f57e93293157961ee87e3dca87a563da5c190
rintaro <136178719+sumirinn@users.noreply.github.com>
2024-12-19 23:31:30 +0000

    [abc342] abc342_e

--- AtCoder submissions/abc342/abc342_e/abc342_e.cpp
+++ AtCoder submissions/abc342/abc342_e/abc342_e.cpp
@@ -2,120 +2,106 @@
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-#define rep(i, n) for (int i = 0; i < (n); i++)
-#define repp(i, n) for (int i = 1; i <= (n); i++)
-#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
-#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
 using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using pdd = pair<double, double>;
-const int inf = 1001001001;
-const ll INF = 8e18;
-using mint = modint998244353;
-//using mint = modint1000000007;
-//a,bが0だと使えないことに注意
+using db = double; 
+using ull = unsigned long long;
+using ld = long double; 
+using bl = bool;
+const int inf = 1001001001; 
+const ll INF = 5e18;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;//大きい順
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;//小さい順
+using vi = vc<int>; using vvi = vv<int>; using vvvi = vv<vi>; using vvvvi = vv<vvi>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vb = vc<bl>; using vvb = vv<bl>; using vvvb = vv<vb>;
+using vdb = vc<db>; using vvdb = vv<db>; using vvvdb = vv<vdb>;
+using vld = vc<ld>; using vvld = vv<ld>; using vvvld = vv<vld>;
+using vs = vc<string>; using vvs = vv<string>;
+#define rep(i,n) for(ll i=0; i<(n); i++)
+#define repp(i,n) for(ll i=1; i<=(n); i++)
+#define drep(i,n) for(ll i=(n)-1; i>=0; i--)
+#define nfor(i,s,n) for(ll i=s; i<n; i++)//i=s,s+1...n-1 ノーマルfor
+#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+template<typename T> void chmax(T& x, T y) {x = max(x, y);}
+template<typename T> void chmin(T& x, T y) {x = min(x, y);}
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define pob pop_back
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define TKAK {cout << "Takahashi" << endl;}else{cout << "Aoki" << endl;}
+#define next_p(v) next_permutation(v.begin(),v.end())
+bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
+    return (!(0 <= i && i < h && 0 <= j && j < w));
+}
 ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
 ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
-//auto nex = calc_next で取得すると楽。
-vector<vector<int> > calc_next(const string &S) {
-    int N = (int)S.size();
-    vector<vector<int> > res(N+1, vector<int>(26, N));
-    for (int i = N-1; i >= 0; --i) {
-        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
-        res[i][S[i]-'a'] = i;
-    }
-    return res;
-}
-//comb(n,k)でnCkになる。
-struct modinv {
-    int n; vector<mint> d;
-    modinv(): n(2), d({0,1}) {}
-    mint operator()(int i) {
-    while (n <= i) d.push_back(-d[mint::mod()%n]*(mint::mod()/n)), ++n;
-    return d[i];
-    }
-    mint operator[](int i) const { return d[i];}
-} invs;
-struct modfact {
-    int n; vector<mint> d;
-    modfact(): n(2), d({1,1}) {}
-    mint operator()(int i) {
-    while (n <= i) d.push_back(d.back()*n), ++n;
-    return d[i];
-    }
-    mint operator[](int i) const { return d[i];}
-} facts;
-struct modfactinv {
-    int n; vector<mint> d;
-    modfactinv(): n(2), d({1,1}) {}
-    mint operator()(int i) {
-    while (n <= i) d.push_back(d.back()*invs(n)), ++n;
-    return d[i];
-    }
-    mint operator[](int i) const { return d[i];}
-} ifacts;
-mint comb(int n, int k) {
-    if (n < k || k < 0) return 0;
-    return facts(n)*ifacts(k)*ifacts(n-k);
-}
-//素数判定  
-bool isPrime(int x){
-    for(int i=2; i*i<=x; i++){
-        if(x%i==0) return false;
-    }
-    return true;
-}
-ll c2(ll n) {return n*(n-1) / 2;}
+ll c2(ll n) {return n*(n-1) / 2;} 
 ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-using P = pair<ll, int>;
-using MP = map<int, vector<int>>;
-void chmax(double& x, double y) {x = max(x, y);} // change max
-void chmin(ll& x, ll y) {x = min(x, y);}
+#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
+#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
+#define fi first
+#define se second
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint; //mint::set_mod(m);で定義できる
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+using vmint = vc<mint>; using vvmint = vv<mint>; using vvvmint = vv<vmint>;
+//using P = pair<db, int>;
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
 const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-//97~122(a~z),65~90(A~Z)
 
-struct edge {
+
+struct Edge{
     int from;
     ll l, d, k, c;
 };
 
+
 int main(){
     int n, m;
     cin >> n >> m;
-    vector<vector<edge>> g(n);
+    vv<Edge> g(n);
     rep(i,m){
-        ll l, d, k, c;
-        int a, b;
-        cin >> l >> d >> k >> c >> a >> b;
-        a--; b--;
-        g[b].push_back(edge{a,l,d,k,c});
+        int l, d, k, c;
+        int u, v;
+        cin >> l >> d >> k >> c >> u >> v;
+        u--; v--;
+        g[v].pb(Edge{u,l,d,k,c});
     }
 
-    vector<ll> f(n, -INF);
+    vl f(n,-INF);
+    pq<pli> q;
     f[n-1] = INF;
-    priority_queue<P> q;
-    q.emplace(INF, n-1);
+    q.emplace(INF,n-1);
+
     while(!q.empty()){
-        auto [t,v] = q.top(); q.pop();
-        if(f[v]!=t) continue;
-        for(auto e : g[v]){
-            ll nt = t - e.c;
-            if(e.l > nt) continue;
-            ll k = (nt - e.l) / e.d;
-            k = min(k, e.k-1);
-            ll nf = e.l + e.d*k;
-            if(f[e.from]>=nf) continue;
-            f[e.from] = nf;
-            q.emplace(nf, e.from);
+        auto [t,now] = q.top();
+        q.pop();
+        if(f[now]!=t) continue;
+        for(auto nex : g[now]){
+            ll nt = t - nex.c;
+            if(nex.l>nt) continue;
+            ll nk = (nt-nex.l)/nex.d;
+            nk = min(nk,nex.k-1);
+            ll nf = nex.l + nex.d*nk;
+            if(f[nex.from]>=nf) continue;
+            f[nex.from] = nf;
+            q.emplace(nf,nex.from);
         }
     }
+
     rep(i,n-1){
         if(f[i]==-INF) cout << "Unreachable" << endl;
-        else cout << f[i] << endl; 
+        else cout << f[i] << endl;
     }
 }
