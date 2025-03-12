cecf7220c28f9b49d5d178bef3a806de7c2fb34d
rintaro <136178719+sumirinn@users.noreply.github.com>
2025-03-12 23:59:17 +0000

    [abc335] abc335_e

--- AtCoder submissions/abc335/abc335_e/abc335_e.cpp
+++ AtCoder submissions/abc335/abc335_e/abc335_e.cpp
@@ -1,67 +1,101 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <stack>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repp(i, n) for (int i = 1; i <= (int)(n); i++)
 using ll = long long;
-using P = pair<int, int>;
+using db = double; 
+using ull = unsigned long long;
+using ld = long double; 
+using bl = bool;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;//大きい順
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;//小さい順
+using vi = vc<int>; using vvi = vv<int>; using vvvi = vv<vi>; using vvvvi = vv<vvi>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vb = vc<bl>; using vvb = vv<bl>; using vvvb = vv<vb>;
+using vdb = vc<db>; using vvdb = vv<db>; using vvvdb = vv<vdb>;
+using vld = vc<ld>; using vvld = vv<ld>; using vvvld = vv<vld>;
+using vs = vc<string>; using vvs = vv<string>;
+#define rep(i,n) for(ll i=0; i<(n); i++)
+#define repp(i,n) for(ll i=1; i<=(n); i++)
+#define drep(i,n) for(ll i=(n)-1; i>=0; i--)
+#define nfor(i,s,n) for(ll i=s; i<n; i++)//i=s,s+1...n-1 ノーマルfor
+#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+template<typename T> void chmax(T& x, T y) {x = max(x, y);}
+template<typename T> void chmin(T& x, T y) {x = min(x, y);}
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define pob pop_back
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define TKAK {cout << "Takahashi" << endl;}else{cout << "Aoki" << endl;}
+#define next_p(v) next_permutation(v.begin(),v.end())
+bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
+    return (!(0 <= i && i < h && 0 <= j && j < w));
+}
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
+#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
+#define fi first
+#define se second
 using mint = modint998244353;
-void chmax(ll& x, ll y) {x = max(x, y);}
-const int di[] = {-1, 0, 1, 0};
-const int dj[] = {0, 1, 0, -1};
-const int inf = 1001001001;
-const ll INF = 1e18;
-ll c3(ll n) { return n*(n-1)*(n-2) / 6; }
-//97~122(a~z),65~90(A~Z)
+//using mint = modint1000000007;
+//using mint = modint; //mint::set_mod(m);で定義できる
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+using vmint = vc<mint>; using vvmint = vv<mint>; using vvvmint = vv<vmint>;
+//using P = pair<db, int>;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
 
 int main(){
-    int n, m; 
+    int n, m;
     cin >> n >> m;
-    vector<int> a(n);
-    rep(i, n) cin >> a[i];
-
+    vi a(n);
+    rep(i,n) cin >> a[i];
     dsu uf(n);
-    vector<P> edges;
-    rep(i, m){
+    vc<pii> edges;
+    rep(mi,m){
         int u, v;
         cin >> u >> v;
         u--; v--;
-        if(a[u]==a[v]) uf.merge(u, v);
-        edges.emplace_back(u, v);
+        if(a[u]==a[v]) uf.merge(u,v);
+        edges.eb(u,v);
     }
-
-    vector<vector<int>> to(n);
-    for(auto [u, v] : edges){
+    vvi to(n);
+    for(auto[u,v] : edges){
         u = uf.leader(u);
         v = uf.leader(v);
-        if(u==v) continue;
-        if(a[u]>a[v]) swap(u, v);
-        to[u].push_back(v);
+        if(a[u]==a[v]) continue;
+        if(a[u]>a[v]) swap(u,v);
+        to[u].pb(v);
     }
 
-    vector<int> vs;
-    rep(i, n) if(uf.leader(i)==i) vs.push_back(i);
-    sort(vs.begin(), vs.end(), [&](int u, int v) {
-        return a[u] < a[v];
+    vi vs;
+    rep(i,n) if(uf.leader(i)==i) vs.pb(i);
+    sort(nall(vs),[&](int u, int v){
+        return a[u]<a[v];
     });
 
-    vector<int> dp(n, -inf);
+    vi dp(n,-inf);
     dp[uf.leader(0)] = 1;
-    for(int v : vs){
-        for(int u : to[v]){
-            dp[u] = max(dp[u], dp[v]+1);
+    for(int now : vs){
+        for(int nex : to[now]){
+            chmax(dp[nex],dp[now]+1); 
         }
     }
-    int ans = dp[uf.leader(n-1)];
-    if(ans<0) ans = 0;
-    cout << ans << endl;
+    cout << max(0,dp[uf.leader(n-1)]) << endl;
 }
