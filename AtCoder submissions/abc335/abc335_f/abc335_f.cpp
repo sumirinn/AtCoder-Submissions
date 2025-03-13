31bcd1153d6e90e51fd1d1979083c3717496806f
rintaro <136178719+sumirinn@users.noreply.github.com>
2025-03-13 00:31:44 +0000

    [abc335] abc335_f

--- AtCoder submissions/abc335/abc335_f/abc335_f.cpp
+++ AtCoder submissions/abc335/abc335_f/abc335_f.cpp
@@ -1,110 +1,84 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <stack>
-#include <set>
-#include <map>
-#include <queue>
-#include <tuple>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 0; i < (n); i++)
-#define repp(i, n) for (int i = 1; i <= (n); i++)
 using ll = long long;
-using MP = map<int, vector<int>>;
+using db = double; 
+using ull = unsigned long long;
+using ld = long double; 
+using bl = bool;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;//大きい順
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;//小さい順
+using vi = vc<int>; using vvi = vv<int>; using vvvi = vv<vi>; using vvvvi = vv<vvi>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vb = vc<bl>; using vvb = vv<bl>; using vvvb = vv<vb>;
+using vdb = vc<db>; using vvdb = vv<db>; using vvvdb = vv<vdb>;
+using vld = vc<ld>; using vvld = vv<ld>; using vvvld = vv<vld>;
+using vs = vc<string>; using vvs = vv<string>;
+#define rep(i,n) for(ll i=0; i<(n); i++)
+#define repp(i,n) for(ll i=1; i<=(n); i++)
+#define drep(i,n) for(ll i=(n)-1; i>=0; i--)
+#define nfor(i,s,n) for(ll i=s; i<n; i++)//i=s,s+1...n-1 ノーマルfor
+#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+template<typename T> void chmax(T& x, T y) {x = max(x, y);}
+template<typename T> void chmin(T& x, T y) {x = min(x, y);}
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define pob pop_back
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define TKAK {cout << "Takahashi" << endl;}else{cout << "Aoki" << endl;}
+#define next_p(v) next_permutation(v.begin(),v.end())
+bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
+    return (!(0 <= i && i < h && 0 <= j && j < w));
+}
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
+#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
+#define fi first
+#define se second
 using mint = modint998244353;
 //using mint = modint1000000007;
-using P = pair<int , int>;
-void chmax(double& x, double y) {x = max(x, y);} // change max
-void chmin(ll& x, ll y) {x = min(x, y);}
+//using mint = modint; //mint::set_mod(m);で定義できる
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+using vmint = vc<mint>; using vvmint = vv<mint>; using vvvmint = vv<vmint>;
+//using P = pair<db, int>;
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
 const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const int inf = 1001001001;
-const ll INF = 1e18;
-ll c2(ll n) {return n*(n-1) / 2;}
-ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-//a,bが0だと使えないことに注意
-ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
-ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
-//auto nex = calc_next で取得すると楽。
-vector<vector<int> > calc_next(const string &S) {
-    int N = (int)S.size();
-    vector<vector<int> > res(N+1, vector<int>(26, N));
-    for (int i = N-1; i >= 0; --i) {
-        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
-        res[i][S[i]-'a'] = i;
-    }
-    return res;
-}
-//comb(n,k)でnCkになる。
-struct modinv {
-    int n; vector<mint> d;
-    modinv(): n(2), d({0,1}) {}
-    mint operator()(int i) {
-    while (n <= i) d.push_back(-d[mint::mod()%n]*(mint::mod()/n)), ++n;
-    return d[i];
-    }
-    mint operator[](int i) const { return d[i];}
-} invs;
-struct modfact {
-    int n; vector<mint> d;
-    modfact(): n(2), d({1,1}) {}
-    mint operator()(int i) {
-    while (n <= i) d.push_back(d.back()*n), ++n;
-    return d[i];
-    }
-    mint operator[](int i) const { return d[i];}
-} facts;
-struct modfactinv {
-    int n; vector<mint> d;
-    modfactinv(): n(2), d({1,1}) {}
-    mint operator()(int i) {
-    while (n <= i) d.push_back(d.back()*invs(n)), ++n;
-    return d[i];
-    }
-    mint operator[](int i) const { return d[i];}
-} ifacts;
-mint comb(int n, int k) {
-    if (n < k || k < 0) return 0;
-    return facts(n)*ifacts(k)*ifacts(n-k);
-}
-//素数判定  
-bool isPrime(int x){
-    for(int i=2; i*i<=x; i++){
-        if(x%i==0) return false;
-    }
-    return true;
-}
-//97~122(a~z),65~90(A~Z)
 
-const int D = 400;
 
 int main(){
     int n;
     cin >> n;
-    vector<int> a(n);
+    vi a(n);
     rep(i,n) cin >> a[i];
 
-    vector<vector<mint>> s(D, vector<mint>(n));
-    vector<mint> dp(n);
+    int d = 400;
+    vv<mint> s(d,vc<mint>(n));
+    vc<mint> dp(n);
     dp[0] = 1;
     rep(i,n){
-        //sの遷移O(√n)
-        rep(j,D){
+        rep(j,d){
             dp[i] += s[j][i];
             if(i+j<n) s[j][i+j] += s[j][i];
         }
-        //a[i]が小さいとき、sで移動O(√n)
-        if(a[i]<D){
+        if(a[i]<d){
             if(i+a[i]<n) s[a[i]][i+a[i]] += dp[i];
         }
-        //a[i]が大きいとき、愚直O(√n)
         else{
             for(int j=i+a[i]; j<n; j+=a[i]){
                 dp[j] += dp[i];
