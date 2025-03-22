96246974e10d5edd9b1d35960369a07f27d96df2
rintaro <136178719+sumirinn@users.noreply.github.com>
2025-03-22 10:33:09 +0000

    [abc332] abc332_d

--- AtCoder submissions/abc332/abc332_d/abc332_d.cpp
+++ AtCoder submissions/abc332/abc332_d/abc332_d.cpp
@@ -1,54 +1,92 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
-#define repp(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
+using db = double; 
+using ull = unsigned long long;
+using ld = long double; 
+using bl = bool;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;//大きい順
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;//小さい順
+using vi = vc<int>; using vvi = vv<int>; using vvvi = vv<vi>; using vvvvi = vv<vvi>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vb = vc<bl>; using vvb = vv<bl>; using vvvb = vv<vb>;
+using vdb = vc<db>; using vvdb = vv<db>; using vvvdb = vv<vdb>;
+using vld = vc<ld>; using vvld = vv<ld>; using vvvld = vv<vld>;
+using vs = vc<string>; using vvs = vv<string>;
+#define rep(i,n) for(ll i=0; i<(n); i++)
+#define repp(i,n) for(ll i=1; i<=(n); i++)
+#define drep(i,n) for(ll i=(n)-1; i>=0; i--)
+#define nfor(i,s,n) for(ll i=s; i<n; i++)//i=s,s+1...n-1 ノーマルfor
+#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+template<typename T> void chmax(T& x, T y) {x = max(x, y);}
+template<typename T> void chmin(T& x, T y) {x = min(x, y);}
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define pob pop_back
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define TKAK {cout << "Takahashi" << endl;}else{cout << "Aoki" << endl;}
+#define next_p(v) next_permutation(v.begin(),v.end())
+bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
+    return (!(0 <= i && i < h && 0 <= j && j < w));
+}
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
+#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
+#define fi first
+#define se second
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint; //mint::set_mod(m);で定義できる
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+using vmint = vc<mint>; using vvmint = vv<mint>; using vvvmint = vv<vmint>;
+//using P = pair<db, int>;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-using vvi = vector<vector<int>>;
 
 int main(){
     int h, w;
     cin >> h >> w;
-    vvi a(h, vector<int>(w));
-    vvi b(h, vector<int>(w));
-    repp(i, h)repp(j, w) cin >> a[i][j];
-    repp(i, h)repp(j, w) cin >> b[i][j];
+    vvi a(h,vi(w)), b(h,vi(w));
+    rep(i,h)rep(j,w) cin >> a[i][j];
+    rep(i,h)rep(j,w) cin >> b[i][j];
 
-    queue<vvi> q;
-    map<vvi, int> dist;
+    vi p(h), q(w);
+    rep(i,h) p[i] = i;
+    rep(j,w) q[j] = j;
 
-    auto push = [&](vvi& s, int d){
-        if(dist.count(s)==1) return;
-        dist[s] = d;
-        q.push(s);
-    };
-
-    push(a, 0);
-
-    while(!q.empty()){
-        vvi s = q.front(); q.pop();
-        int d = dist[s];
-        repp(i, h-1){
-            vvi ns = s;
-            swap(ns[i], ns[i+1]);
-            push(ns, d+1);
-        }
-        repp(j, w-1){
-            vvi ns = s;
-            repp(i, h) swap(ns[i][j], ns[i][j+1]);
-            push(ns, d+1);
-        }
-    }
-
-    if(dist.count(b)==1) cout << dist[b] << endl;
-    else cout << -1 << endl;
+    int ans = inf;
+    do{
+        do{
+            bool ok = true;
+            rep(i,h)rep(j,w){
+                if(a[p[i]][q[j]]!=b[i][j]) ok = false;
+            }
+            if(ok){
+                int cnt = 0;
+                rep(i1,h)rep(i2,h) if(i1<i2 && p[i1]>p[i2]) cnt++;
+                rep(j1,w)rep(j2,w) if(j1<j2 && q[j1]>q[j2]) cnt++;
+                chmin(ans,cnt);
+            }
+        }while(next_p(q));
+    }while(next_p(p));
+    if(ans==inf) ans = -1;
+    cout << ans << endl;
 }
