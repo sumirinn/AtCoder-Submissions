a8e44b254b268c102f7c37a1585bb5d02f16e95f
rintaro <136178719+sumirinn@users.noreply.github.com>
2025-04-02 10:33:56 +0000

    [abc327] abc327_d

--- AtCoder submissions/abc327/abc327_d/abc327_d.cpp
+++ AtCoder submissions/abc327/abc327_d/abc327_d.cpp
@@ -1,43 +1,95 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
-#define repp(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
-//97~122
+using db = double; 
+using ull = unsigned long long;
+using ld = long double; 
+using bl = bool;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;//大きい順
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;//小さい順
+using vi = vc<int>; using vvi = vv<int>; using vvvi = vv<vi>; using vvvvi = vv<vvi>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vb = vc<bl>; using vvb = vv<bl>; using vvvb = vv<vb>;
+using vdb = vc<db>; using vvdb = vv<db>; using vvvdb = vv<vdb>;
+using vld = vc<ld>; using vvld = vv<ld>; using vvvld = vv<vld>;
+using vs = vc<string>; using vvs = vv<string>;
+#define rep(i,n) for(ll i=0; i<(n); i++)
+#define repp(i,n) for(ll i=1; i<=(n); i++)
+#define drep(i,n) for(ll i=(n)-1; i>=0; i--)
+#define nfor(i,s,n) for(ll i=s; i<n; i++)//i=s,s+1...n-1 ノーマルfor
+#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+template<typename T> void chmax(T& x, T y) {x = max(x, y);}
+template<typename T> void chmin(T& x, T y) {x = min(x, y);}
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define pob pop_back
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define TKAK {cout << "Takahashi" << endl;}else{cout << "Aoki" << endl;}
+#define next_p(v) next_permutation(v.begin(),v.end())
+bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
+    return (!(0 <= i && i < h && 0 <= j && j < w));
+}
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
+#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
+#define fi first
+#define se second
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint; //mint::set_mod(m);で定義できる
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+using vmint = vc<mint>; using vvmint = vv<mint>; using vvvmint = vv<vmint>;
+//using P = pair<db, int>;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
 
 int main(){
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> to(n);
-    vector<int> a(m), b(m);
-    repp(i,m) cin >> a[i], a[i]--;
-    repp(i,m) cin >> b[i], b[i]--;
-    repp(i,m){
-        to[a[i]].push_back(b[i]);
-        to[b[i]].push_back(a[i]);
-    }
+	int n, m;
+	cin >> n >> m;
+	vi a(m), b(m);
+	rep(i,m) cin >> a[i];
+	rep(i,m) cin >> b[i];
+	vvi to(n);
+	rep(i,m) {
+		a[i]--; b[i]--;
+		to[a[i]].pb(b[i]);
+		to[b[i]].pb(a[i]);
+	}
 
-    vector<int> color(n, -1);
-    auto dfs = [&](auto dfs, int v, int c) -> void {
-        if(color[v]!=-1) return;
-        color[v] = c;
-        for(int u : to[v]) dfs(dfs, u, 1-c);
-    };
+	vi used(n,-1);
+	bool ok = true;
+	auto dfs = [&](auto dfs, int now, int pre, int col)->void{
+		used[now] = col;
+		for(auto nex : to[now]){
+			if(used[nex]!=-1){
+				if(used[nex]!=1-col){
+					ok = false;
+					return;
+				}
+				continue;
+			}
+			dfs(dfs,nex,now,1-col);
+		}
+	};
 
-    repp(i, n) dfs(dfs, i, 0);
-
-    repp(i, m) if(color[a[i]]==color[b[i]]){
-        cout << "No" << endl;
-        return 0;
-    }
-    cout << "Yes" << endl;
+	rep(i,n)if(used[i]==-1) dfs(dfs,i,-1,0);
+	if(ok) YN;
 }
