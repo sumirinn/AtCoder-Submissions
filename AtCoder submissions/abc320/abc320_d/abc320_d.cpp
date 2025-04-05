87c604142e7343ca414b6fb8d61ed79e534cf82b
rintaro <136178719+sumirinn@users.noreply.github.com>
2025-04-05 11:06:22 +0000

    [abc320] abc320_d

--- AtCoder submissions/abc320/abc320_d/abc320_d.cpp
+++ AtCoder submissions/abc320/abc320_d/abc320_d.cpp
@@ -1,57 +1,98 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
-#define repp(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
-using P = pair<ll, ll>;
-//97~122(a~z)
+using db = double; 
+using ull = unsigned long long;
+using ld = long double; 
+using bl = bool;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;//大きい順
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;//小さい順
+using vi = vc<int>; using vvi = vv<int>; using vvvi = vv<vi>; using vvvvi = vv<vvi>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vb = vc<bl>; using vvb = vv<bl>; using vvvb = vv<vb>;
+using vdb = vc<db>; using vvdb = vv<db>; using vvvdb = vv<vdb>;
+using vld = vc<ld>; using vvld = vv<ld>; using vvvld = vv<vld>;
+using vs = vc<string>; using vvs = vv<string>;
+#define rep(i,n) for(ll i=0; i<(n); i++)
+#define repp(i,n) for(ll i=1; i<=(n); i++)
+#define drep(i,n) for(ll i=(n)-1; i>=0; i--)
+#define nfor(i,s,n) for(ll i=s; i<n; i++)//i=s,s+1...n-1 ノーマルfor
+#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+template<typename T> void chmax(T& x, T y) {x = max(x, y);}
+template<typename T> void chmin(T& x, T y) {x = min(x, y);}
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define pob pop_back
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define TKAK {cout << "Takahashi" << endl;}else{cout << "Aoki" << endl;}
+#define next_p(v) next_permutation(v.begin(),v.end())
+bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
+    return (!(0 <= i && i < h && 0 <= j && j < w));
+}
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
+#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
+#define fi first
+#define se second
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint; //mint::set_mod(m);で定義できる
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+using vmint = vc<mint>; using vvmint = vv<mint>; using vvvmint = vv<vmint>;
+//using P = pair<db, int>;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
 
 int main(){
-    int n, m;
-    cin >> n >> m;
+	int n, m;
+	cin >> n >> m;
 
-    map<pair<int, int>, P> go;
-    int a, b;
-    ll X, Y;
+	map<pii,pll> l;
+	vvi to(n);
+	rep(mi,m){
+		int u, v;
+		ll x, y;
+		cin >> u >> v >> x >> y;
+		u--; v--;
+		l[pii(u,v)] = pii(x,y);
+		l[pii(v,u)] = pii(-x,-y);
+		to[u].pb(v);
+		to[v].pb(u);
+	}
 
-    bool visited[200009];
-    rep(i, n) visited[i] = false;
+	vc<pll> dist(n,pll(INF,INF));
+	dist[0] = pll(0,0);
+	auto dfs = [&](auto dfs, int now)->void{
+		auto [x,y] = dist[now];
+		for(int nex : to[now]){
+			if(dist[nex]!=pll(INF,INF)) continue;
+			auto [nx,ny] = l[pii(now,nex)];
+			dist[nex] = pll(x+nx,y+ny);
+			dfs(dfs,nex);
+		}
+	};
+	dfs(dfs,0);
 
-    vector<int> g[200009];
-
-    rep(i, m){
-        cin >> a >> b >> X >> Y;  
-        go[make_pair(a, b)] = make_pair(X, Y);
-        go[make_pair(b, a)] = make_pair(-X, -Y);
-        g[a].push_back(b);
-        g[b].push_back(a);
-    }
-
-    P POS[200009];
-    POS[0] = P(0, 0);
-
-    auto dfs = [&](auto dfs, int pos) -> void {
-        visited[pos] = true;
-        repp(i, g[pos].size()){
-            int nex = g[pos][i];
-            POS[nex] = make_pair(POS[pos].first + go[make_pair(pos, nex)].first, POS[pos].second + go[make_pair(pos, nex)].second);
-            if(!(visited[nex])) dfs(dfs, nex);
-        }
-    };
-
-    dfs(dfs, 1);
-
-    rep(i, n){
-        if(visited[i]) cout << POS[i].first << " " << POS[i].second << endl;
-        else cout << "undecidable" << endl;
-    }
+	rep(i,n){
+		if(dist[i]==pll(INF,INF)) cout << "undecidable" << endl;
+		else cout << dist[i].fi << " " << dist[i].se << endl;
+	}
 }
