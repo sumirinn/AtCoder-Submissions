2d59a4bcb6f945de3c1686a0437acf60cd5d00e1
rintaro <136178719+sumirinn@users.noreply.github.com>
2025-04-08 13:05:11 +0000

    [abc174] abc174_d

--- AtCoder submissions/abc174/abc174_d/abc174_d.cpp
+++ AtCoder submissions/abc174/abc174_d/abc174_d.cpp
@@ -2,74 +2,80 @@
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-#define rep(i, n) for (int i = 0; i < (n); i++)
-#define repp(i, n) for (int i = 1; i <= (n); i++)
-#define rep2(i, a, b) for(int i = (a); i <= (b); i++)
-#define rep3(i, a, b, c) for(int i = (a); i <= (b); i+=(c))
+using ll = long long;
+using db = double; 
+using ull = unsigned long long;
+using ld = long double; 
+using bl = bool;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;//大きい順
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;//小さい順
+using vi = vc<int>; using vvi = vv<int>; using vvvi = vv<vi>; using vvvvi = vv<vvi>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vb = vc<bl>; using vvb = vv<bl>; using vvvb = vv<vb>;
+using vdb = vc<db>; using vvdb = vv<db>; using vvvdb = vv<vdb>;
+using vld = vc<ld>; using vvld = vv<ld>; using vvvld = vv<vld>;
+using vs = vc<string>; using vvs = vv<string>;
+#define rep(i,n) for(ll i=0; i<(n); i++)
+#define repp(i,n) for(ll i=1; i<=(n); i++)
+#define drep(i,n) for(ll i=(n)-1; i>=0; i--)
+#define nfor(i,s,n) for(ll i=s; i<n; i++)//i=s,s+1...n-1 ノーマルfor
+#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+template<typename T> void chmax(T& x, T y) {x = max(x, y);}
+template<typename T> void chmin(T& x, T y) {x = min(x, y);}
 #define pb push_back
 #define eb emplace_back
-#define mp make_pair
-#define fi first
-#define se second
-using ll = long long; using db = double;
-using ull = unsigned long long;
-using pii = pair<int, int>; using pll = pair<ll, ll>;  
-using pdd = pair<double, double>; using pli = pair<ll, int>;
-using pil = pair<int, ll>;
-const int inf = 1001001001; const ll INF = 3e18;
-using mint = modint998244353;
-//using mint = modint1000000007;
-//using mint = modint;
-//mint::set_mod(m);で定義できる。
-//a,bが0だと使えないことに注意
+#define em emplace
+#define pob pop_back
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define TKAK {cout << "Takahashi" << endl;}else{cout << "Aoki" << endl;}
+#define next_p(v) next_permutation(v.begin(),v.end())
+bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
+    return (!(0 <= i && i < h && 0 <= j && j < w));
+}
 ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
 ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-// res[i][c] := i 文字目以降で最初に文字 c が登場する index (存在しないときは n)
-//auto nex = calc_next で取得すると楽。
-vector<vector<int> > calc_next(const string &S) {
-    int N = (int)S.size();
-    vector<vector<int> > res(N+1, vector<int>(26, N));
-    for (int i = N-1; i >= 0; --i) {
-        for (int j = 0; j < 26; ++j) res[i][j] = res[i+1][j];
-        res[i][S[i]-'a'] = i;
-    }
-    return res;
-}
-ll c2(ll n) {return n*(n-1) / 2;}
+ll c2(ll n) {return n*(n-1) / 2;} 
 ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-using P = pair<string, int>;
-using MP = map<int, vector<int>>;
-using C = complex<double>;
-C inC(){
-    double x, y;
-    cin >> x >> y;
-    return C(x,y);
-}
-struct Edge {
-    int u, v;
-    ll w;
-    Edge(){}
-    Edge(int u, int v, ll w): u(u), v(v), w(w){}
-};
-void chmax(ll& x, ll y) {x = max(x, y);} // change max
-void chmin(ll& x, ll y) {x = min(x, y);}
+#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
+#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
+#define fi first
+#define se second
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint; //mint::set_mod(m);で定義できる
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+using vmint = vc<mint>; using vvmint = vv<mint>; using vvvmint = vv<vmint>;
+//using P = pair<db, int>;
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
 const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-//97~122(a~z),65~90(A~Z)
 
 
 int main(){
     int n;
+    cin >> n;
     string s;
-    cin >> n >> s;
+    cin >> s;
 
     int cnt = 0;
-    rep(i,n) if(s[i]=='R') cnt++;
-
+    rep(i,n)if(s[i]=='R') cnt++;
     int ans = 0;
-    rep(i,cnt) if(s[i]=='W') ans++;
-
+    rep(i,n){
+        if(s[i]=='R') cnt--;
+        if(s[i]=='W'&&cnt>0){
+            cnt--;
+            ans++;
+        }
+    }
     cout << ans << endl;
 }
