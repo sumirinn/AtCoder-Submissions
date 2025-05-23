b0a507ab0b4a5501c571b15aa762d706bce9915b
rintaro <136178719+sumirinn@users.noreply.github.com>
2025-05-23 08:55:55 +0000

    [abc301] abc301_c

--- AtCoder submissions/abc301/abc301_c/abc301_c.cpp
+++ AtCoder submissions/abc301/abc301_c/abc301_c.cpp
@@ -1,48 +1,94 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-
-#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
-#define repp(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
+using db = double; 
+using ull = unsigned long long;
+using ld = long double; 
+using bl = bool;
+const int inf = 1001001001; 
+const ll INF = 3e18;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;//大きい順
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;//小さい順
+using vi = vc<int>; using vvi = vv<int>; using vvvi = vv<vi>; using vvvvi = vv<vvi>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vb = vc<bl>; using vvb = vv<bl>; using vvvb = vv<vb>;
+using vdb = vc<db>; using vvdb = vv<db>; using vvvdb = vv<vdb>;
+using vld = vc<ld>; using vvld = vv<ld>; using vvvld = vv<vld>;
+using vs = vc<string>; using vvs = vv<string>;
+#define rep(i,n) for(ll i=0; i<(n); i++)
+#define repp(i,n) for(ll i=1; i<=(n); i++)
+#define drep(i,n) for(ll i=(n)-1; i>=0; i--)
+#define nfor(i,s,n) for(ll i=s; i<n; i++)//i=s,s+1...n-1 ノーマルfor
+#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+template<typename T> void chmax(T& x, T y) {x = max(x, y);}
+template<typename T> void chmin(T& x, T y) {x = min(x, y);}
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define pob pop_back
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define TKAK {cout << "Takahashi" << endl;}else{cout << "Aoki" << endl;}
+#define next_p(v) next_permutation(v.begin(),v.end())
+bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
+    return (!(0 <= i && i < h && 0 <= j && j < w));
+}
+ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
+ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
+ll c2(ll n) {return n*(n-1) / 2;} 
+ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
+#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
+#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
+#define fi first
+#define se second
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint; //mint::set_mod(m);で定義できる
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>; 
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+using vmint = vc<mint>; using vvmint = vv<mint>; using vvvmint = vv<vmint>;
+//using P = pair<db, int>;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-//97~122
-string s, t;
 
 int main(){
+    string s, t;
     cin >> s >> t;
+    int n = s.size();
+    string u = "atcoder";
 
-    int cnts[100], cntt[100];
-    repp(i, 50) cnts[i] = 0; 
-    repp(i, 50) cntt[i] = 0;
-    repp(i, s.size()){
-        if(s[i]=='@') cnts[0]++;
-        else cnts[int(s[i])-96]++;
-    }
-    repp(i, t.size()){
-        if(t[i]=='@') cntt[0]++;
-        else cntt[int(t[i])-96]++;
+    int m = 26;
+    vi cnt1(m), cnt2(m);
+    int sa=0, ta=0;
+    rep(i,n){
+        if(s[i]=='@') sa++;
+        else cnt1[s[i]-'a']++;
+        if(t[i]=='@') ta++;
+        else cnt2[t[i]-'a']++;
     }
 
-    bool ans = true;
-    rep(i, 26){
-        if(i==int('a')-96 || i==int('t')-96 || i==int('c')-96 || i==int('o')-96 || i==int('d')-96 || i==int('e')-96 || i==int('r')-96 ){
-            if(cnts[i]<cntt[i]) cnts[0] -= (cntt[i] - cnts[i]);
-            if(cnts[i]>cntt[i]) cntt[0] -= (cnts[i] - cntt[i]);
-        }else{
-            if(cnts[i]!=cntt[i]) ans = false;
+    bool ok = true;
+    rep(i,m){
+        bool c = true;
+        rep(j,7)if(i==u[j]-'a') c = false;
+        if(c && cnt1[i] != cnt2[i]){
+            ok = false;
+            break;
         }
+        if(cnt1[i] < cnt2[i]) sa -= cnt2[i]-cnt1[i];
+        else ta -= cnt1[i]-cnt2[i];
     }
 
-    if(cnts[0]<0 || cntt[0]<0) ans =false;
-
-    if(ans) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    if(sa<0 || ta<0) ok = false;
+    if(ok) YN;
 }
