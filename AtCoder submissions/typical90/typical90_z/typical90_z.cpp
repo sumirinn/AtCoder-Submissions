80a7d92cea741e2646d233b7ca8b93ac8d2cdc2b
rintaro <136178719+sumirinn@users.noreply.github.com>
2025-09-26 15:58:00 +0000

    [typical90] typical90_z

--- AtCoder submissions/typical90/typical90_z/typical90_z.cpp
+++ AtCoder submissions/typical90/typical90_z/typical90_z.cpp
@@ -2,40 +2,71 @@
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-#define rep(i, n) for (int i = 0; i < (n); i++)
-#define repp(i, n) for (int i = 1; i <= (n); i++)
+using ll = long long;
+using db = double;
+using ull = unsigned long long;
+using ld = long double;
+using bl = bool;
+const int inf = 1001001001;
+const ll INF = 3e18;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;//大きい順
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;//小さい順
+using vi = vc<int>; using vvi = vv<int>; using vvvi = vv<vi>; using vvvvi = vv<vvi>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vb = vc<bl>; using vvb = vv<bl>; using vvvb = vv<vb>;
+using vdb = vc<db>; using vvdb = vv<db>; using vvvdb = vv<vdb>;
+using vld = vc<ld>; using vvld = vv<ld>; using vvvld = vv<vld>;
+using vs = vc<string>; using vvs = vv<string>;
+#define rep(i,n) for(ll i=0; i<(n); i++)
+#define repp(i,n) for(ll i=1; i<=(n); i++)
+#define drep(i,n) for(ll i=(n)-1; i>=0; i--)
+#define nfor(i,s,n) for(ll i=s; i<n; i++)//i=s,s+1...n-1 ノーマルfor
+#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+template<typename T> void chmax(T& x, T y) {x = max(x, y);}
+template<typename T> void chmin(T& x, T y) {x = min(x, y);}
 #define pb push_back
 #define eb emplace_back
-#define fi first
-#define se second
-using ll = long long; using db = double; using ull = unsigned long long;
-using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<double, double>; 
-using pli = pair<ll, int>; using pil = pair<int, ll>;
-const int inf = 1001001001; 
-const ll INF = 3e18;
-using mint = modint998244353;
-//using mint = modint1000000007;
-//using mint = modint;
-//mint::set_mod(m);で定義できる
-//a,bが0だと使えないことに注意
+#define em emplace
+#define pob pop_back
+#define YES cout << "Yes" << endl;
+#define NO cout << "No" << endl;
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define TKAK {cout << "Takahashi" << endl;}else{cout << "Aoki" << endl;}
+#define next_p(v) next_permutation(v.begin(),v.end())
+bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
+    return (!(0 <= i && i < h && 0 <= j && j < w));
+}
 ll gcd(ll a, ll b) {if(a%b==0)return b; else return gcd(b, a%b);}
 ll lcm(ll a, ll b) {return a*b / gcd(a, b);}
-ll c2(ll n) {return n*(n-1) / 2;} 
+ll c2(ll n) {return n*(n-1) / 2;}
 ll c3(ll n) {return n*(n-1)*(n-2) / 6;}
-//using P = pair<double, int>;
-using C = complex<double>;
-void chmax(ll& x, ll y) {x = max(x, y);} 
-void chmin(ll& x, ll y) {x = min(x, y);}
+#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
+#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
+#define fi first
+#define se second
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint; //mint::set_mod(m);で定義できる
+using pii = pair<int, int>; using pll = pair<ll, ll>; using pdd = pair<db, db>;
+using pli = pair<ll, int>; using pil = pair<int, ll>;
+using vmint = vc<mint>; using vvmint = vv<mint>; using vvvmint = vv<vmint>;
+//using P = pair<db, int>;
 const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, -1, 0, 1};
 const int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
 
+
+
 int main(){
     int n;
     cin >> n;
-    vector<vector<int>> to(n);
+    vvi to(n);
     rep(i,n-1){
         int u, v;
         cin >> u >> v;
@@ -44,28 +75,35 @@ int main(){
         to[v].pb(u);
     }
 
-    set<int> st;
-    auto dfs =[&](auto dfs, int now, int pre, bool pre_c)->void{
-        bool now_c = !pre_c;
-        if(now_c) st.insert(now);
+    int st = -1;
+    rep(i,n)if(to[i].size()==1) st = i;
+
+    set<int> ans;
+    auto dfs = [&](auto dfs, int now, int pre, bool ok) -> void {
+        if(ok) ans.insert(now);
+        bool nok = !ok;
         for(int nex : to[now]){
             if(nex==pre) continue;
-            dfs(dfs,nex,now,now_c);
+            dfs(dfs,nex,now,nok);
         }
     };
-    dfs(dfs,0,-1,false);
+    dfs(dfs,st,-1,true);
 
-    if(st.size()<n/2){
-        set<int> st2;
-        swap(st2,st);
-        for(int i=0; i<n; i++) if(!st2.count(i)) st.insert(i);
-    }
-
-    int cnt = n/2;
-    for(int e : st){
-        cout << e + 1 << " ";
-        cnt--;
-        if(cnt==0) break;
+    if(ans.size()>=n/2){
+        int cnt = 0;
+        for(int e : ans){
+            if(cnt>=n/2) continue;
+            cout << e+1 << " ";
+            cnt++;
+        }
+    }else{
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(ans.count(i)) continue;
+            if(cnt>=n/2) continue;
+            cout << i + 1 << endl;
+            cnt++;
+        }
     }
     cout << endl;
 }
